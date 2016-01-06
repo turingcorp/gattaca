0e461a557eaa1b31a5220220f0191ce309d9d011
vauxhall <zero.griffin@gmail.com>
2016-01-06 16:16:46 -0600

    vbrowse

---
+++ gattaca/sourceteq/source/v/browse/vbrowse.h
@@ -0,0 +1,5 @@
+#import "appdel.h"
+
+@interface vbrowse:UIView
+
+@end
---
+++ gattaca/sourceteq/source/v/browse/vbrowse.m
@@ -0,0 +1,13 @@
+#import "vbrowse.h"
+
+@implementation vbrowse
+
+-(instancetype)init
+{
+    self = [super init];
+    [self setBackgroundColor:[UIColor whiteColor]];
+    
+    return self;
+}
+
+@end
