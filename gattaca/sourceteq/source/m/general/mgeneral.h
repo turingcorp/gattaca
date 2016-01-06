7729df05fdfebd70c1bb261aa089ec3ba5de0e41
vauxhall <zero.griffin@gmail.com>
2016-01-06 14:56:40 -0600

    wip

---
+++ gattaca/sourceteq/source/m/general/mgeneral.h
@@ -0,0 +1,6 @@
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger, ga_screen)
+{
+    ga_screen_home
+};
--- gattaca/sourceteq/source/m/m.h
+++ gattaca/sourceteq/source/m/m.h
@@ -1,13 +1,1 @@
-//
-//  m.h
-//  gattaca
-//
-//  Created by zero on 1/6/16.
-//  Copyright © 2016 Iturbide. All rights reserved.
-//
-
-#ifndef m_h
-#define m_h
-
-
-#endif /* m_h */
+#import "mgeneral.h"
