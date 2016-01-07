5c5a30cb3535fafb6fbdccc80fa507ced2744a7e
vauxhall <zero.griffin@gmail.com>
2016-01-06 18:14:16 -0600

    msection is factory for msectionbase

---
+++ gattaca/sourceteq/source/m/general/msection.h
@@ -0,0 +1,13 @@
+//
+//  msection.h
+//  gattaca
+//
+//  Created by zero on 1/6/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface msection : NSObject
+
+@end
