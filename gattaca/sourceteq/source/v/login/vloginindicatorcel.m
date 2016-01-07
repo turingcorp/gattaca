7493ab653b2b8fbb633ade0b8960f4d41ca7e6dc
vauxhall <zero.griffin@gmail.com>
2016-01-07 09:22:27 -0600

    circles in indicator cel

--- gattaca/sourceteq/source/v/login/vloginindicatorcel.m
+++ gattaca/sourceteq/source/v/login/vloginindicatorcel.m
@@ -3,20 +3,39 @@
 @implementation vloginindicatorcel
 {
     UIColor *fillcollor;
+    CGRect circlerect;
 }
 
 -(instancetype)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     [self setBackgroundColor:[UIColor clearColor]];
+    [self hover];
     
+    circlerect = CGRectMake(3, 3, frame.size.width - 6, frame.size.height - 6);
     
     return self;
 }
 
 -(void)drawRect:(CGRect)rect
 {
-    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetStrokeColorWithColor(context, [UIColor whiteColor].CGColor);
+    CGContextSetFillColorWithColor(context, fillcollor.CGColor);
+    CGContextAddEllipseInRect(context, circlerect);
+    CGContextDrawPath(context, kCGPathFillStroke);
+}
+
+-(void)setSelected:(BOOL)selected
+{
+    [super setSelected:selected];
+    [self hover];
+}
+
+-(void)setHighlighted:(BOOL)highlighted
+{
+    [super setHighlighted:highlighted];
+    [self hover];
 }
 
 #pragma mark functionality
