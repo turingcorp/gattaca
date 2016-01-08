fb8d6373f7a56575f60aad526972b91748f5a510
vauxhall <zero.griffin@gmail.com>
2016-01-07 18:50:49 -0600

    working on config view

--- gattaca/sourceteq/source/v/config/vconfig.m
+++ gattaca/sourceteq/source/v/config/vconfig.m
@@ -6,8 +6,35 @@
 {
     self = [super init:controller];
     [self setBackgroundColor:[UIColor redColor]];
+
+    UICollectionViewFlowLayout *flow = [[UICollectionViewFlowLayout alloc] init];
+    [flow setFooterReferenceSize:CGSizeZero];
+    [flow setMinimumInteritemSpacing:0];
+    [flow setMinimumLineSpacing:10];
+    [flow setScrollDirection:UICollectionViewScrollDirectionVertical];
+    [flow setSectionInset:UIEdgeInsetsMake(10, 0, 0, 40)];
+    
+    UICollectionView *collection = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:flow];
+    [collection setBackgroundColor:[UIColor clearColor]];
+    [collection setShowsHorizontalScrollIndicator:NO];
+    [collection setShowsVerticalScrollIndicator:NO];
+    
+    [self addSubview:collection];
     
     return self;
+}
+
+#pragma mark -
+#pragma mark col del
+
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView*)col
+{
+    return 1;
+}
+
+-(NSInteger)collectionView:(UICollectionView*)col numberOfItemsInSection:(NSInteger)section
+{
+    return 0;
 }
 
 @end
