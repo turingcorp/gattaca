6255b0f6712c7175eabe67cb23f9041cdf4e561c
vauxhall <zero.griffin@gmail.com>
2016-01-12 08:21:38 -0600

    queries

--- gattaca/sourceteq/source/m/myprofile/mmyprofile.m
+++ gattaca/sourceteq/source/m/myprofile/mmyprofile.m
@@ -22,12 +22,24 @@
 
 -(void)loaduser
 {
-    
+    NSString *query = @"SELECT name, namestr, age, latitude, longitude FROM profile";
 }
 
 -(void)firsttime
 {
+    self.latitude = 0;
+    self.longitude = 0;
+    self.age = 50;
+    self.nametype = profile_name_firstname;
+    self.namestr = @"";
     
+    NSInteger now = [NSDate date].timeIntervalSince1970;
+    NSString *query = [NSString stringWithFormat:
+                       @"INSERT INTO profile (created, syncstamp, name, namestr, age, latitude, longitude) "
+                       "VALUES(%@, 0, %@, \"%@\", %@, %@, %@);",
+                       @(now), @(self.nametype), self.namestr, @(self.age), @(self.latitude), @(self.longitude)];
+    
+    [db query:query];
 }
 
 @end
