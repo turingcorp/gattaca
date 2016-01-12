af2d4911fa9ed82a9d1162d62c8b597492df42e4
vauxhall <zero.griffin@gmail.com>
2016-01-12 08:29:43 -0600

    taking out coords from db

--- gattaca/sourceteq/source/m/myprofile/mmyprofile.m
+++ gattaca/sourceteq/source/m/myprofile/mmyprofile.m
@@ -25,7 +25,7 @@
 
 -(void)loaduser
 {
-    NSString *query = @"SELECT name, namestr, age, latitude, longitude FROM profile limit 1;";
+    NSString *query = @"SELECT name, namestr, age FROM profile limit 1;";
     NSDictionary *rawuser = [db row:query];
     
     if(rawuser)
@@ -33,8 +33,6 @@
         self.nametype = (profile_name)[rawuser[@"name"] integerValue];
         self.namestr = rawuser[@"namestr"];
         self.age = [rawuser[@"age"] integerValue];
-        self.latitude = [rawuser[@"latitude"] integerValue];
-        self.longitude = [];
     }
     else
     {
@@ -44,17 +42,15 @@
 
 -(void)firsttime
 {
-    self.latitude = 0;
-    self.longitude = 0;
     self.age = 50;
     self.nametype = profile_name_firstname;
     self.namestr = NSLocalizedString(@"profile_default_user", nil);
     
     NSInteger now = [NSDate date].timeIntervalSince1970;
     NSString *query = [NSString stringWithFormat:
-                       @"INSERT INTO profile (created, syncstamp, name, namestr, age, latitude, longitude) "
-                       "VALUES(%@, 0, %@, \"%@\", %@, %@, %@);",
-                       @(now), @(self.nametype), self.namestr, @(self.age), @(self.latitude), @(self.longitude)];
+                       @"INSERT INTO profile (created, syncstamp, name, namestr, age) "
+                       "VALUES(%@, 0, %@, \"%@\", %@);",
+                       @(now), @(self.nametype), self.namestr, @(self.age)];
     
     [db query:query];
 }
