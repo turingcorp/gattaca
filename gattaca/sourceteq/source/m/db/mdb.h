db9081ac39188ad6ce8db11a51c1b7ba98edb73b
vauxhall <zero.griffin@gmail.com>
2016-01-11 20:29:54 -0600

    db functions

---
+++ gattaca/sourceteq/source/m/db/mdb.h
@@ -0,0 +1,13 @@
+//
+//  mdb.h
+//  gattaca
+//
+//  Created by zero on 1/11/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mdb : NSObject
+
+@end
---
+++ gattaca/sourceteq/source/m/db/mdbcreate.h
@@ -0,0 +1,13 @@
+//
+//  mdbcreate.h
+//  gattaca
+//
+//  Created by zero on 1/11/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mdbcreate : NSObject
+
+@end
