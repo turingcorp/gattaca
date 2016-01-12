c69f1a46ab5573db98fc7c359d8f9be938098a7e
vauxhall <zero.griffin@gmail.com>
2016-01-12 13:27:59 -0600

    fb permisions

--- gattaca/sourceteq/source/m/myprofile/mmyprofile.h
+++ gattaca/sourceteq/source/m/myprofile/mmyprofile.h
@@ -9,6 +9,7 @@
 
 @property(copy, nonatomic)NSString *namestr;
 @property(nonatomic)profile_name nametype;
+@property(nonatomic)profile_gender gender;
 @property(nonatomic)NSInteger latitude;
 @property(nonatomic)NSInteger longitude;
 @property(nonatomic)NSInteger age;
