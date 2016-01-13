3b671aa92ad510399462d912ccee20d07ed4731b
vauxhall <zero.griffin@gmail.com>
2016-01-13 11:34:45 -0600

    slash

--- gattaca/sourceteq/source/v/profile/vprofilecircleslash.h
+++ gattaca/sourceteq/source/v/profile/vprofilecircleslash.h
@@ -2,9 +2,12 @@
 
 @interface vprofilecircleslash:UIView
 
+-(instancetype)init:(CGFloat)linewidth radius:(CGFloat)radius start:(CGFloat)start end:(CGFloat)end;
+
 @property(nonatomic)CGFloat linewidth;
-@property(nonatomic)CGFloat startpoint;
-@property(nonatomic)CGFloat endpoint;
+@property(nonatomic)CGFloat linewidth_2;
 @property(nonatomic)CGFloat radius;
+@property(nonatomic)CGFloat start;
+@property(nonatomic)CGFloat end;
 
 @end
--- gattaca/sourceteq/source/v/profile/vprofilecircleslash.m
+++ gattaca/sourceteq/source/v/profile/vprofilecircleslash.m
@@ -2,23 +2,48 @@
 
 @implementation vprofilecircleslash
 
--(instancetype)init
+-(instancetype)init:(CGFloat)linewidth radius:(CGFloat)radius start:(CGFloat)start end:(CGFloat)end
 {
     self = [super init];
  
     [self setBackgroundColor:[UIColor clearColor]];
     [self setTranslatesAutoresizingMaskIntoConstraints:NO];
     
+    self.linewidth = linewidth;
+    self.linewidth_2 = linewidth / 2.0;
+    self.radius = radius;
+    self.start = start;
+    self.end = end;
+    
     return self;
 }
 
 -(void)drawRect:(CGRect)rect
 {
+    CGFloat width_2 = rect.size.width / 2.0;
+    CGFloat height_2 = rect.size.height / 2.0;
+    CGPoint pointstart;
+    CGPoint pointend;
+    
     CGContextRef context = UIGraphicsGetCurrentContext();
     CGContextSetLineWidth(context, self.linewidth);
     CGContextSetStrokeColorWithColor(context, self.tintColor.CGColor);
-    CGContextAddArc(context, rect.size.width / 2, rect.size.height / 2, self.radius, self.startpoint, self.endpoint, 0);
+    CGContextSetFillColorWithColor(context, self.tintColor.CGColor);
+    CGContextAddArc(context, width_2, height_2, self.radius, self.start, self.end, 0);
+    pointend = CGContextGetPathCurrentPoint(context);
     CGContextDrawPath(context, kCGPathStroke);
+    CGContextAddArc(context, width_2, height_2, self.radius, self.end, self.start, 0);
+    pointstart = CGContextGetPathCurrentPoint(context);
+    CGContextAddEllipseInRect(context, [self rectwithpoint:pointstart]);
+    CGContextAddEllipseInRect(context, [self rectwithpoint:pointend]);
+    CGContextDrawPath(context, kCGPathFill);
+}
+
+#pragma mark functionality
+
+-(CGRect)rectwithpoint:(CGPoint)point
+{
+    return CGRectMake(point.x - self.linewidth_2, point.y - self.linewidth_2, point.x + self.linewidth_2, point.y + self.linewidth_2);
 }
 
 @end
