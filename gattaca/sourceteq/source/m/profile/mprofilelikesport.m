094ead4e4ed839096c44901604448db6c14325c7
vauxhall <zero.griffin@gmail.com>
2016-01-13 09:26:20 -0600

    profiles

--- gattaca/sourceteq/source/m/profile/mprofilelikesport.m
+++ gattaca/sourceteq/source/m/profile/mprofilelikesport.m
@@ -1,5 +1,40 @@
 #import "mprofilelikesport.h"
 
 @implementation mprofilelikesport
+{
+    NSInteger current;
+}
+
+-(instancetype)init:(NSInteger)amount
+{
+    self = [super init];
+    
+    current = amount;
+    
+    return self;
+}
+
+#pragma mark -
+#pragma mark like protocol
+
+-(profile_like)type
+{
+    return profile_like_sport;
+}
+
+-(UIColor*)color
+{
+    return [UIColor colorWithRed:1 green:0.2 blue:0 alpha:1];
+}
+
+-(NSInteger)currentamount
+{
+    return current;
+}
+
+-(void)newamount:(NSInteger)newamount
+{
+    current = newamount;
+}
 
 @end
