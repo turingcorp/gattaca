f48f7d162915e9976ffeb07959dab8a4401a60a8
vauxhall <zero.griffin@gmail.com>
2016-01-13 08:45:02 -0600

    like & ground protocol

--- gattaca/sourceteq/source/m/profile/mprofilegroundprotocol.h
+++ gattaca/sourceteq/source/m/profile/mprofilegroundprotocol.h
@@ -1,5 +1,10 @@
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @protocol mprofilegroundprotocol <NSObject>
+
+-(instancetype)init:(NSInteger)amount;
+-(profile_ground)type;
+-(NSInteger)amount;
+-(UIColor*)color;
 
 @end
--- gattaca/sourceteq/source/m/profile/mprofilelikeprotocol.h
+++ gattaca/sourceteq/source/m/profile/mprofilelikeprotocol.h
@@ -1,9 +1,10 @@
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @protocol mprofilelikeprotocol <NSObject>
 
 -(instancetype)init:(NSInteger)amount;
 -(profile_like)type;
 -(NSInteger)amount;
+-(UIColor*)color;
 
 @end
