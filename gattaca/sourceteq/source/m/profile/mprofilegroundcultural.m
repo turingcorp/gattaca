08302500791ec12d9347887f646c89f02702fee5
vauxhall <zero.griffin@gmail.com>
2016-01-13 09:31:16 -0600

    grounds

--- gattaca/sourceteq/source/m/profile/mprofilegroundcultural.m
+++ gattaca/sourceteq/source/m/profile/mprofilegroundcultural.m
@@ -1,5 +1,40 @@
 #import "mprofilegroundcultural.h"
 
 @implementation mprofilegroundcultural
+{
+    NSInteger current;
+}
+
+-(instancetype)init:(NSInteger)amount
+{
+    self = [super init];
+    
+    current = amount;
+    
+    return self;
+}
+
+#pragma mark -
+#pragma mark ground protocol
+
+-(profile_ground)type
+{
+    return profile_ground_cultural;
+}
+
+-(UIColor*)color
+{
+    return [UIColor colorWithRed:0.3 green:0.5 blue:0.7 alpha:1];
+}
+
+-(NSInteger)currentamount
+{
+    return current;
+}
+
+-(void)newamount:(NSInteger)newamount
+{
+    current = newamount;
+}
 
 @end
