b6b430e6c405d5265927a12c467e7b3e0048b7b0
vauxhall <zero.griffin@gmail.com>
2016-01-12 18:24:55 -0600

    collection

--- gattaca/sourceteq/source/v/profile/vprofileage.m
+++ gattaca/sourceteq/source/v/profile/vprofileage.m
@@ -1,6 +1,10 @@
 #import "vprofileage.h"
 
 @implementation vprofileage
+{
+    NSInteger celwidth;
+    NSInteger verticalmargin;
+}
 
 -(instancetype)init:(cprofileage*)controller
 {
@@ -8,6 +12,10 @@
     [self setBackgroundColor:[UIColor clearColor]];
     
     vblur *blur = [vblur light];
+    celwidth = 100;
+    verticalmargin = 20;
+    NSInteger celheight = 50;
+    NSInteger colheight = celheight + (verticalmargin * 2);
     
     UIButton *btnaccept = [[UIButton alloc] init];
     [btnaccept setBackgroundColor:colormain];
@@ -20,16 +28,39 @@
     [btnaccept setTranslatesAutoresizingMaskIntoConstraints:NO];
     [btnaccept addTarget:self action:@selector(actionaccept) forControlEvents:UIControlEventTouchUpInside];
     
+    UICollectionViewFlowLayout *flow = [[UICollectionViewFlowLayout alloc] init];
+    [flow setFooterReferenceSize:CGSizeZero];
+    [flow setHeaderReferenceSize:CGSizeZero];
+    [flow setItemSize:CGSizeMake(celwidth, celheight)];
+    [flow setMinimumInteritemSpacing:0];
+    [flow setMinimumLineSpacing:0];
+    [flow setScrollDirection:UICollectionViewScrollDirectionHorizontal];
+    
+    UICollectionView *collection = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:flow];
+    [collection setClipsToBounds:YES];
+    [collection setBackgroundColor:[UIColor redColor]];
+    [collection setShowsHorizontalScrollIndicator:NO];
+    [collection setShowsVerticalScrollIndicator:NO];
+    [collection setAlwaysBounceHorizontal:YES];
+    [collection setDataSource:self];
+    [collection setDelegate:self];
+    [collection registerClass:[vprofileagecel class] forCellWithReuseIdentifier:celid];
+    [collection setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
     [self addSubview:blur];
     [self addSubview:btnaccept];
+    [self addSubview:collection];
     
-    NSDictionary *views = @{@"blur":blur, @"btn":btnaccept};
-    NSDictionary *metrics = @{};
+    NSDictionary *views = @{@"blur":blur, @"btn":btnaccept, @"col":collection};
+    NSDictionary *metrics = @{@"colheight":@(colheight)};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[blur]-0-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[blur]-0-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[btn]-20-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[btn(36)]-20-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[col(colheight)]-20-[btn(36)]-20-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[col]-0-|" options:0 metrics:metrics views:views]];
+    
+    self.ages = [[mmyprofileages alloc] init];
     
     return self;
 }
@@ -44,6 +75,13 @@
 #pragma mark -
 #pragma mark col del
 
+-(UIEdgeInsets)collectionView:(UICollectionView*)col layout:(UICollectionViewLayout*)layout insetForSectionAtIndex:(NSInteger)section
+{
+    CGFloat hrmargin = (self.bounds.size.width - celwidth) / 2;
+    
+    return UIEdgeInsetsMake(verticalmargin, hrmargin, verticalmargin, hrmargin);
+}
+
 -(NSInteger)numberOfSectionsInCollectionView:(UICollectionView*)col
 {
     return 1;
@@ -56,9 +94,10 @@
 
 -(UICollectionViewCell*)collectionView:(UICollectionView*)col cellForItemAtIndexPath:(NSIndexPath*)index
 {
+    vprofileagecel *cel = [col dequeueReusableCellWithReuseIdentifier:celid forIndexPath:index];
+    [cel config:[self.ages item:index.item]];
     
-    
-    return nil;
+    return cel;
 }
 
 @end
