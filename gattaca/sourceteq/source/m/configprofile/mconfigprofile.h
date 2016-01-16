c8edeabe53fe437dc7c9a90d66d5b05fb7cca99e
vauxhall <zero.griffin@gmail.com>
2016-01-15 20:39:23 -0600

    config profile

---
+++ gattaca/sourceteq/source/m/configprofile/mconfigprofile.h
@@ -0,0 +1,13 @@
+//
+//  mconfigprofile.h
+//  gattaca
+//
+//  Created by zero on 1/15/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mconfigprofile : NSObject
+
+@end
---
+++ gattaca/sourceteq/source/m/configprofile/mconfigprofile.m
@@ -0,0 +1,13 @@
+//
+//  mconfigprofile.m
+//  gattaca
+//
+//  Created by zero on 1/15/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "mconfigprofile.h"
+
+@implementation mconfigprofile
+
+@end
