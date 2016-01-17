1db6fc43f3213c42b327377e3254006710829407
vauxhall <zero.griffin@gmail.com>
2016-01-17 15:02:30 -0600

    constraints

--- gattaca/sourceteq/source/v/configprofile/vconfigprofilecel.m
+++ gattaca/sourceteq/source/v/configprofile/vconfigprofilecel.m
@@ -9,12 +9,21 @@
     [self setBackgroundColor:[UIColor whiteColor]];
     
     UILabel *title = [[UILabel alloc] init];
+    self.title = title;
     
     UILabel *value = [[UILabel alloc] init];
-    
+    self.value = value;
     
     [self addSubview:title];
     [self addSubview:value];
+    
+    NSDictionary *views = @{@"title":title, @"value":value};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[title]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[value]-20-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-10-[title]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[value]-0-|" options:0 metrics:metrics views:views]];
     
     return self;
 }
