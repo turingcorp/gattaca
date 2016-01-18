e0f9e5fee9a2ac5c711ccbcf14bdd39cff1ab682
vauxhall <zero.griffin@gmail.com>
2016-01-18 09:09:12 -0600

    cel

--- gattaca/sourceteq/source/v/configprofile/vconfigprofilecel.m
+++ gattaca/sourceteq/source/v/configprofile/vconfigprofilecel.m
@@ -6,14 +6,12 @@
 {
     self = [super initWithFrame:frame];
     [self setClipsToBounds:YES];
-    [self setBackgroundColor:[UIColor whiteColor]];
     
     UILabel *title = [[UILabel alloc] init];
     [title setBackgroundColor:[UIColor clearColor]];
     [title setFont:[UIFont fontWithName:fontname size:14]];
     [title setUserInteractionEnabled:NO];
     [title setTranslatesAutoresizingMaskIntoConstraints:NO];
-    [title setTextColor:[UIColor colorWithWhite:0 alpha:0.7]];
     self.title = title;
     
     UILabel *value = [[UILabel alloc] init];
@@ -22,7 +20,6 @@
     [value setUserInteractionEnabled:NO];
     [value setTextAlignment:NSTextAlignmentRight];
     [value setTranslatesAutoresizingMaskIntoConstraints:NO];
-    [value setTextColor:[UIColor colorWithWhite:0 alpha:0.4]];
     self.value = value;
     
     UIImageView *icon = [[UIImageView alloc] init];
@@ -41,12 +38,46 @@
     NSDictionary *views = @{@"title":title, @"value":value, @"icon":icon};
     NSDictionary *metrics = @{};
     
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[title]" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-15-[title]" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[value]-60-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-5-[title]" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[value]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[icon(30)]-10-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[icon]-0-|" options:0 metrics:metrics views:views]];
     
     return self;
+}
+
+-(void)setSelected:(BOOL)selected
+{
+    [super setSelected:selected];
+    [self hover];
+}
+
+-(void)setHighlighted:(BOOL)highlighted
+{
+    [super setHighlighted:highlighted];
+    [self hover];
+}
+
+#pragma mark functionality
+
+-(void)hover
+{
+    if(self.isSelected || self.isHighlighted)
+    {
+        [self.title setTextColor:[UIColor whiteColor]];
+        [self.value setTextColor:[UIColor whiteColor]];
+        [self.icon setTintColor:[UIColor whiteColor]];
+        [self setBackgroundColor:colormain];
+    }
+    else
+    {
+        [self.title setTextColor:[UIColor colorWithWhite:0 alpha:0.7]];
+        [self.value setTextColor:[UIColor colorWithWhite:0 alpha:0.35]];
+        [self.icon setTintColor:[UIColor colorWithWhite:0 alpha:0.35]];
+        [self setBackgroundColor:[UIColor whiteColor]];
+    }
 }
 
 #pragma mark public
@@ -55,6 +86,7 @@
 {
     [self.title setText:[item title]];
     [self.value setText:[item value]];
+    [self hover];
 }
 
 @end
