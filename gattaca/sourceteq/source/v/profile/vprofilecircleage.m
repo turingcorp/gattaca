3371f5763f2dea122d18326766c9582a4b3d8341
vauxhall <zero.griffin@gmail.com>
2016-01-18 14:02:09 -0600

    circle age

--- gattaca/sourceteq/source/v/profile/vprofilecircleage.m
+++ gattaca/sourceteq/source/v/profile/vprofilecircleage.m
@@ -2,4 +2,15 @@
 
 @implementation vprofilecircleage
 
+-(instancetype)init:(vprofilecircle*)circle
+{
+    self = [super init];
+    [self setBackgroundColor:[UIColor clearColor]];
+    [self setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    self.circle = circle;
+    
+    return self;
+}
+
 @end
--- gattaca/sourceteq/source/v/profile/vprofilecircleribbon.m
+++ gattaca/sourceteq/source/v/profile/vprofilecircleribbon.m
@@ -32,6 +32,10 @@
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-160-[lbl]-160-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-60-[ribbon]-60-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-150-[ribbon]-150-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-70-[lbl]-70-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-160-[lbl]-160-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-60-[ribbon]-60-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-150-[ribbon]-150-|" options:0 metrics:metrics views:views]];
     
     return self;
 }
