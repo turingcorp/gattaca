7a0a9d374a2615a3728898c99078e9a19fb600da
vauxhall <zero.griffin@gmail.com>
2016-01-18 14:45:08 -0600

    age in profile circle

--- gattaca/sourceteq/source/v/profile/vprofilecircleage.m
+++ gattaca/sourceteq/source/v/profile/vprofilecircleage.m
@@ -12,17 +12,19 @@
     
     UILabel *lbl = [[UILabel alloc] init];
     [lbl setBackgroundColor:[UIColor clearColor]];
-    [lbl setFont:[UIFont fontWithName:fontboldname size:16]];
+    [lbl setFont:[UIFont fontWithName:fontboldname size:20]];
     [lbl setTextAlignment:NSTextAlignmentCenter];
-    [lbl setTranslatesAutoresizingMaskIntoConstraints:NO];
-    [lbl setTextColor:[UIColor colorWithWhite:0 alpha:0.4]];
+    [lbl setTextColor:[UIColor colorWithWhite:0 alpha:0.25]];
     [lbl setText:[NSString stringWithFormat:@"%@", @([mmyprofile singleton].profile.age)]];
+    [lbl setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    [self addSubview:lbl];
     
     NSDictionary *views = @{@"lbl":lbl};
     NSDictionary *metrics = @{};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[lbl]-0-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[lbl]-100-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[lbl]-110-|" options:0 metrics:metrics views:views]];
     
     return self;
 }
