59aac4d8c5f44006e86952f67ab54c3906d27377
vauxhall <zero.griffin@gmail.com>
2016-01-19 08:52:03 -0600

    header and cel

--- gattaca/sourceteq/source/v/gattaca/vgattacatester.m
+++ gattaca/sourceteq/source/v/gattaca/vgattacatester.m
@@ -9,4 +9,22 @@
     return self;
 }
 
+#pragma mark -
+#pragma mark col del
+
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView*)col
+{
+    return 1;
+}
+
+-(NSInteger)collectionView:(UICollectionView*)col numberOfItemsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+-(UICollectionViewCell*)collectionView:(UICollectionView*)col cellForItemAtIndexPath:(NSIndexPath*)index
+{
+    return cel;
+}
+
 @end
