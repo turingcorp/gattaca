0c4894efd5b0b2ef8745b7b5eba0c7bbcaabb16e
vauxhall <zero.griffin@gmail.com>
2017-08-07 17:02:06 -0500

    Create extension factory

--- gattaca/Model/Home/MHomeGiphy.swift
+++ gattaca/Model/Home/MHomeGiphy.swift
@@ -2,5 +2,33 @@ import Foundation
 
 extension MHome
 {
+    private static let kResourceName:String = "ResourceURL"
+    private static let kResourceExtension:String = "plist"
     
+    func requestGif()
+    {
+        
+    }
+    
+    //MARK: private
+    
+    private class func factoryUrlMap() -> [String:AnyObject]?
+    {
+        guard
+            
+            let resourceUrl:URL = Bundle.main.url(
+                forResource:kResourceName,
+                withExtension:kResourceExtension),
+            let urlDictionary:NSDictionary = NSDictionary(
+                contentsOf:resourceUrl)
+            
+        else
+        {
+            return nil
+        }
+        
+        let urlMap:[String:AnyObject]? = urlDictionary as? [String:AnyObject]
+        
+        return urlMap
+    }
 }
