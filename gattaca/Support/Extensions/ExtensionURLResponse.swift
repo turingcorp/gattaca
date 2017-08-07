8789d5f361911c6aec3e32539cfd65b2d018b36d
vauxhall <zero.griffin@gmail.com>
2017-08-07 18:18:01 -0500

    Get status code from response

--- gattaca/Support/Extensions/ExtensionURLResponse.swift
+++ gattaca/Support/Extensions/ExtensionURLResponse.swift
@@ -2,5 +2,20 @@ import Foundation
 
 extension URLResponse
 {
-    
+    var statusCode:Int?
+    {
+        get
+        {
+            guard
+                
+                let response:HTTPURLResponse = self as? HTTPURLResponse
+                
+            else
+            {
+                return nil
+            }
+            
+            return response.statusCode
+        }
+    }
 }
