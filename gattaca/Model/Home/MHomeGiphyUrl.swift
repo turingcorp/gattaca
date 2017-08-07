9c2eb54512ba6c768a30a3bc299a0395c8293a4b
vauxhall <zero.griffin@gmail.com>
2017-08-07 17:19:11 -0500

    Factory random url

--- gattaca/Model/Home/MHomeGiphyUrl.swift
+++ gattaca/Model/Home/MHomeGiphyUrl.swift
@@ -2,5 +2,35 @@ import Foundation
 
 class MHomeGiphyUrl
 {
+    private static let kKeyRoot:String = "giphy"
+    private static let kKeyHost:String = "host"
+    private static let kKeyRandom:String = "random"
     
+    class func factoryRandom() -> URL?
+    {
+        guard
+        
+            let urlMap:[String:AnyObject] = MHome.factoryUrlMap(),
+            let giphyRoot:[String:AnyObject] = urlMap[kKeyRoot] as? [String:AnyObject],
+            let host:String = giphyRoot[kKeyHost] as? String,
+            let random:String = giphyRoot[kKeyRandom] as? String
+        
+        else
+        {
+            return nil
+        }
+        
+        let pathString:String = "\(host)\(random)"
+        
+        guard
+        
+            let url:URL = URL(string:pathString)
+        
+        else
+        {
+            return nil
+        }
+        
+        return url
+    }
 }
