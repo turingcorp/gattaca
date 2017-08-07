ce071a48b46f6901024e554ad83e0eb9744eeb9e
vauxhall <zero.griffin@gmail.com>
2017-08-07 17:07:50 -0500

    Create class giphy url

--- gattaca/Controller/Home/CHome.swift
+++ gattaca/Controller/Home/CHome.swift
@@ -2,5 +2,10 @@ import UIKit
 
 class CHome:Controller<VHome, MHome>
 {
-    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        model.requestGif()
+    }
 }
--- gattaca/Model/Home/MHomeGiphy.swift
+++ gattaca/Model/Home/MHomeGiphy.swift
@@ -2,33 +2,28 @@ import Foundation
 
 extension MHome
 {
-    private static let kResourceName:String = "ResourceURL"
-    private static let kResourceExtension:String = "plist"
-    
     func requestGif()
     {
-        
+        DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
+        { [weak self] in
+            
+            self?.dispatchRequestGif()
+        }
     }
     
     //MARK: private
     
-    private class func factoryUrlMap() -> [String:AnyObject]?
+    private func dispatchRequestGif()
     {
         guard
-            
-            let resourceUrl:URL = Bundle.main.url(
-                forResource:kResourceName,
-                withExtension:kResourceExtension),
-            let urlDictionary:NSDictionary = NSDictionary(
-                contentsOf:resourceUrl)
-            
+        
+            let urlMap:[String:AnyObject] = MHome.factoryUrlMap()
+        
         else
         {
-            return nil
+            return
         }
         
-        let urlMap:[String:AnyObject]? = urlDictionary as? [String:AnyObject]
         
-        return urlMap
     }
 }
