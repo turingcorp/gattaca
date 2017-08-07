2e0845c66e4cec747ecf54e5da5bf3168b29d601
vauxhall <zero.griffin@gmail.com>
2017-08-07 15:41:38 -0500

    Layout view bar

--- gattaca/View/Home/VHome.swift
+++ gattaca/View/Home/VHome.swift
@@ -2,13 +2,45 @@ import UIKit
 
 class VHome:ViewMain
 {
+    private let kBarHeight:CGFloat = 70
+    
     required init(controller:UIViewController)
     {
         super.init(controller:controller)
+        
+        guard
+        
+            let controller:CHome = controller as? CHome
+        
+        else
+        {
+            return
+        }
+        
+        factoryViews(controller:controller)
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    //MARK: private
+    
+    private func factoryViews(controller:CHome)
+    {
+        let viewBar:VHomeBar = VHomeBar(controller:controller)
+        
+        addSubview(viewBar)
+        
+        NSLayoutConstraint.bottomToBottom(
+            view:viewBar,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:viewBar,
+            constant:kBarHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewBar,
+            toView:self)
     }
 }
