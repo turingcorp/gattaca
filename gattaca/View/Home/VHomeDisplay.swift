788a46e3b8293f5eb6ca8dda1fa1b25778540998
vauxhall <zero.griffin@gmail.com>
2017-08-07 20:14:09 -0500

    Create extension data

--- gattaca/View/Home/VHomeDisplay.swift
+++ gattaca/View/Home/VHomeDisplay.swift
@@ -1,14 +1,15 @@
 import UIKit
+import GifHero
 
 class VHomeDisplay:View<VHome, MHome, CHome>
 {
-    
+    private weak var viewGif:GifView?
     
     required init(controller:CHome)
     {
         super.init(controller:controller)
         isUserInteractionEnabled = false
-        backgroundColor = UIColor.colourBackgroundDark
+        backgroundColor = UIColor.black
     }
     
     required init?(coder:NSCoder)
@@ -16,10 +17,38 @@ class VHomeDisplay:View<VHome, MHome, CHome>
         return nil
     }
     
+    //MARK: private
+    
+    private func factoryGif()
+    {
+        guard
+            
+            let item:MHomeItem = controller.model.items.first
+        
+        else
+        {
+            return
+        }
+        
+        let url:URL = item.url
+        
+        let viewGif:GifView = GifView()
+        viewGif.url = url
+        viewGif.animating = true
+        self.viewGif = viewGif
+        
+        addSubview(viewGif)
+        
+        NSLayoutConstraint.equals(
+            view:viewGif,
+            toView:self)
+    }
+    
     //MARK: public
     
     func refresh()
     {
-        
+        viewGif?.removeFromSuperview()
+        factoryGif()
     }
 }
