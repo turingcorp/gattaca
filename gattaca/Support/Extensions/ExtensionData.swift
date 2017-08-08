85668db8810fbae876921186436534d3e85ef5a4
vauxhall <zero.griffin@gmail.com>
2017-08-07 20:16:06 -0500

    Add write to temporal

--- gattaca/Support/Extensions/ExtensionData.swift
+++ gattaca/Support/Extensions/ExtensionData.swift
@@ -2,5 +2,21 @@ import Foundation
 
 extension Data
 {
-    
+    func writeToTemporal() -> URL?
+    {
+        let randomName:String = UUID().uuidString
+        let directoryUrl:URL = URL(fileURLWithPath:NSTemporaryDirectory())
+        let fileUrl:URL = directoryUrl.appendingPathComponent(randomName)
+        
+        do
+        {
+            try write(to:fileUrl, options:Data.WritingOptions.atomicWrite)
+        }
+        catch
+        {
+            return nil
+        }
+        
+        return fileUrl
+    }
 }
