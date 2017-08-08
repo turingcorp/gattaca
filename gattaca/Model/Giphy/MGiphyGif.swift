a09e91bbe045ac50b800fc015db6e7125ba6507f
vauxhall <zero.griffin@gmail.com>
2017-08-08 12:36:00 -0500

    Try fixed height

--- gattaca/Model/Giphy/MGiphyGif.swift
+++ gattaca/Model/Giphy/MGiphyGif.swift
@@ -4,6 +4,7 @@ extension MGiphy
 {
     private static let kKeyData:String = "data"
     private static let kKeyOriginalUrl:String = "image_original_url"
+    private static let kKeyFixedHeight:String = "fixed_height"
     
     class func factoryGif(json:Any) -> URLRequest?
     {
@@ -16,6 +17,8 @@ extension MGiphy
             return nil
         }
         
+        print(url)
+        
         let request:URLRequest = MRequest.factoryGetRequest(url:url)
         
         return request
@@ -25,18 +28,20 @@ extension MGiphy
     
     private class func factoryUrl(json:Any) -> URL?
     {
+        print(json)
+        
         guard
         
             let jsonMap:[String:AnyObject] = json as? [String:AnyObject],
             let data:[String:AnyObject] = jsonMap[kKeyData] as? [String:AnyObject],
-            let originalUrl:String = data[kKeyOriginalUrl] as? String
+            let fixedHeight:String = data[kKeyFixedHeight] as? String
         
         else
         {
             return nil
         }
         
-        let forcingSsl:String = MRequest.forceSsl(path:originalUrl)
+        let forcingSsl:String = MRequest.forceSsl(path:fixedHeight)
         let url:URL? = URL(string:forcingSsl)
         
         return url
