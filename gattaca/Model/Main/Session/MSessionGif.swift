a0d97eea337f1d2937052dd480e0c84289b496e8
vauxhall <zero.griffin@gmail.com>
2017-08-09 13:40:54 -0500

    Add look for indexes

--- gattaca/Model/Main/Session/MSessionGif.swift
+++ gattaca/Model/Main/Session/MSessionGif.swift
@@ -15,12 +15,73 @@ class MSessionGif
         indexWaiting = 0
     }
     
+    //MARK: private
+    
+    private func lookIndexes()
+    {
+        var itemIndex:Int = 0
+        var indexLoading:Int?
+        var indexWaiting:Int?
+        
+        for item:DGif in items
+        {
+            let status:DGif.Status = item.status
+            
+            switch status
+            {
+            case DGif.Status.new,
+                 DGif.Status.loading:
+                
+                if indexLoading == nil
+                {
+                    indexLoading = itemIndex
+                }
+                
+                break
+                
+            case DGif.Status.waiting:
+                
+                if indexWaiting == nil
+                {
+                    indexWaiting = itemIndex
+                }
+                
+                break
+                
+            default:
+                break
+            }
+            
+            itemIndex += 1
+            
+            if indexLoading != nil
+            {
+                break
+            }
+        }
+        
+        indexesFound(indexLoading:indexLoading, indexWaiting:indexWaiting)
+    }
+    
+    private func indexesFound(indexLoading:Int?, indexWaiting:Int?)
+    {
+        if let indexLoading:Int = indexLoading
+        {
+            self.indexLoading = indexLoading
+        }
+        
+        if let indexWaiting:Int = indexWaiting
+        {
+            self.indexWaiting = indexWaiting
+        }
+    }
+    
     //MARK: internal
     
     func itemsLoaded(items:[DGif])
     {
         self.items = items
-        
+        lookIndexes()
     }
     
     func currentItem() -> DGif?
