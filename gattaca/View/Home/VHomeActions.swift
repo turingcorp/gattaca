53bb078e5232e8f02a6097f25a4cf8e6a3bb039c
vauxhall <zero.griffin@gmail.com>
2017-08-08 22:54:11 -0500

    Add collection delegate methods

--- gattaca/View/Home/VHomeActions.swift
+++ gattaca/View/Home/VHomeActions.swift
@@ -6,5 +6,32 @@ class VHomeActions:VCollection<
     CHome,
     VHomeActionsCell>
 {
+    private var cellSize:CGSize?
+    private let kCellWidth:CGFloat = 80
     
+    required init(controller:CHome)
+    {
+        super.init(controller:controller)
+        collectionView.bounces = false
+        collectionView.isScrollEnabled = false
+        
+        if let flow:VCollectionFlow = collectionView.collectionViewLayout as? VCollectionFlow
+        {
+            flow.scrollDirection = UICollectionViewScrollDirection.horizontal
+        }
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        numberOfItemsInSection section:Int) -> Int
+    {
+        let count:Int = controller.model.actions.count
+        
+        return count count
+    }
 }
