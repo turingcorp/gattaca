1b6c0b48d3f20a180de574f42d6693e567d9046c
vauxhall <zero.griffin@gmail.com>
2017-08-09 00:29:25 -0500

    Create action like

---
+++ gattaca/Model/Home/MHomeActionLike.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class MHomeActionLike:MHomeActionProtocol
+{
+    var icon:UIImage
+    {
+        get
+        {
+            return #imageLiteral(resourceName: "assetGenericActionLike")
+        }
+    }
+}
