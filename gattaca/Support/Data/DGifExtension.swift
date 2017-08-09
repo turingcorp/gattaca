fed8ceb28cb679acd232398a84317076b7ad87b2
vauxhall <zero.griffin@gmail.com>
2017-08-09 10:58:28 -0500

    Add enums for states

--- gattaca/Support/Data/DGifExtension.swift
+++ gattaca/Support/Data/DGifExtension.swift
@@ -2,5 +2,20 @@ import Foundation
 
 extension DGif
 {
+    enum Mark:Int16
+    {
+        case none
+        case hate
+        case meh
+        case like
+    }
     
+    enum Status:Int16
+    {
+        case new
+        case loading
+        case waiting
+        case marked
+        case cleaned
+    }
 }
