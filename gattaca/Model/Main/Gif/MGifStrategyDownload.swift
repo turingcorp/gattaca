01415638c18550cbfd3e733ca34090961b63841b
vauxhall <zero.griffin@gmail.com>
2017-08-09 15:27:07 -0500

    Add notification gif loaded

--- gattaca/Model/Main/Gif/MGifStrategyDownload.swift
+++ gattaca/Model/Main/Gif/MGifStrategyDownload.swift
@@ -2,5 +2,21 @@ import Foundation
 
 class MGifStrategyDownload:MGifStrategy
 {
+    override init(model:MGif)
+    {
+        super.init(model:model)
+        
+        DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
+        { [weak self] in
+            
+            self?.dispatchDownload()
+        }
+    }
     
+    //MARK: private
+    
+    private func dispatchDownload()
+    {
+        
+    }
 }
--- gattaca/Support/Extensions/ExtensionNotification.swift
+++ gattaca/Support/Extensions/ExtensionNotification.swift
@@ -3,4 +3,5 @@ import Foundation
 extension Notification
 {
     static let sessionLoaded:Notification.Name = Notification.Name("sessionLoaded")
+    static let gifDownloaded:Notification.Name = Notification.Name("gifDownloaded")
 }
