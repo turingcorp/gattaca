735ba7ebe80d3512767831b2de87fa86a1063e53
vauxhall <zero.griffin@gmail.com>
2017-08-09 00:18:11 -0500

    Add background to cell

--- gattaca/View/Home/VHomeActionsCell.swift
+++ gattaca/View/Home/VHomeActionsCell.swift
@@ -2,5 +2,26 @@ import UIKit
 
 class VHomeActionsCell:UICollectionViewCell
 {
+    private weak var background:UIImageView!
     
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        
+        let background:UIImageView = UIImageView()
+        background.clipsToBounds = true
+        background.contentMode = UIViewContentMode.center
+        background.translatesAutoresizingMaskIntoConstraints = false
+        background.isUserInteractionEnabled = false
+        self.background = background
+        
+        addSubview(background)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
