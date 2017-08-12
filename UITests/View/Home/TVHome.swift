c807ab474aa49b88628ef5a6c23e449d5d121919
vauxhall <zero.griffin@gmail.com>
2017-08-12 10:33:33 -0500

    Create test view home

---
+++ UITests/View/Home/TVHome.swift
@@ -0,0 +1,13 @@
+import XCTest
+
+class TVHome:XCTestCase
+{
+    override func setUp()
+    {
+        super.setUp()
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+    
+    
+}
