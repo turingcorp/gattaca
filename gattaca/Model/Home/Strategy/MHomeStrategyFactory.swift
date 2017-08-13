e66657500930e4e67464fcba24e8395ed4b3dabb
vauxhall <zero.griffin@gmail.com>
2017-08-13 11:01:28 -0500

    Factory strategy

--- gattaca/Model/Home/Strategy/MHomeStrategyFactory.swift
+++ gattaca/Model/Home/Strategy/MHomeStrategyFactory.swift
@@ -2,34 +2,37 @@ import Foundation
 
 extension MHomeStrategy
 {
-    class func factoryStrategy(controller:CHome) -> MHomeStrategy?
-    {
-        return nil
-        /*
-        let map:[MSession.Status:MHomeStrategy.Type] = strategyMap()
-        let currentStatus:MSession.Status = MSession.sharedInstance.status
-        
-        guard
-        
-            let strategyType:MHomeStrategy.Type = map[currentStatus]
-        
-        else
-        {
-            return nil
-        }
-        
-        let strategy:MHomeStrategy = strategyType.init(
-            controller:controller)
-        
-        return strategy*/
-    }
-    
     //MARK: private
     
     private class func strategyMap() -> [MSession.Status:MHomeStrategy.Type]
     {
-        let map:[MSession.Status:MHomeStrategy.Type] = [:]
+        let map:[MSession.Status:MHomeStrategy.Type] = [
+            MSession.Status.new:MHomeStrategyNew.self,
+            MSession.Status.loading:MHomeStrategyLoading.self,
+            MSession.Status.loaded:MHomeStrategyLoaded.self]
         
         return map
+    }
+    
+    //MARK: internal
+    
+    class func factoryStrategy(controller:CHome) -> MHomeStrategy?
+    {
+         let map:[MSession.Status:MHomeStrategy.Type] = strategyMap()
+         let currentStatus:MSession.Status = controller.model.session.status
+         
+         guard
+         
+             let strategyType:MHomeStrategy.Type = map[currentStatus]
+         
+         else
+         {
+             return nil
+         }
+         
+         let strategy:MHomeStrategy = strategyType.init(
+            controller:controller)
+         
+         return strategy
     }
 }
