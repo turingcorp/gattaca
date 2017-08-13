6af985a8d57b83106129a9dc07a79dc01a9ef03b
vauxhall <zero.griffin@gmail.com>
2017-08-13 10:01:02 -0500

    Remove notification session loaded

--- gattaca/Support/Extensions/ExtensionNotification.swift
+++ gattaca/Support/Extensions/ExtensionNotification.swift
@@ -2,6 +2,5 @@ import Foundation
 
 extension Notification
 {
-    static let sessionLoaded:Notification.Name = Notification.Name("sessionLoaded")
     static let gifDownloaded:Notification.Name = Notification.Name("gifDownloaded")
 }
