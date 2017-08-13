16e6a87e896ea5f5724d2591ff1f3173ea9a6ec4
vauxhall <zero.griffin@gmail.com>
2017-08-13 11:53:36 -0500

    Add test to create session

--- UnitTests/Model/Session/TMSessionLoader.swift
+++ UnitTests/Model/Session/TMSessionLoader.swift
@@ -1,6 +1,31 @@
 import XCTest
+@testable import gattaca
 
 class TMSessionLoader:XCTestCase
 {
+    private let kWaitExpectation:TimeInterval = 3
     
+    func testCreate()
+    {
+        let sessionExpectation:XCTestExpectation = expectation(
+            description:"session created")
+        
+        let manager:DManager? = DManager()
+        
+        XCTAssertNotNil(
+            manager,
+            "failed creating manager")
+        
+        let session:MSession = MSession()
+        session.create(manager:manager!)
+        { (session:DSession) in
+            
+            sessionExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout:kWaitExpectation)
+        { (error:Error?) in
+            
+        }
+    }
 }
