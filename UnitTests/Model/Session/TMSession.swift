7bab049f09bdc4312bdd4dd39c51033a23367d5b
vauxhall <zero.griffin@gmail.com>
2017-08-13 11:41:44 -0500

    Test loading session

--- UnitTests/Model/Session/TMSession.swift
+++ UnitTests/Model/Session/TMSession.swift
@@ -1,6 +1,27 @@
 import XCTest
+@testable import gattaca
 
 class TMSession:XCTestCase
 {
+    private let kWaitExpectation:TimeInterval = 3
     
+    func testLoad()
+    {
+        let sessionExpectation:XCTestExpectation = expectation(
+            description:"session loaded")
+        
+        let session:MSession = MSession()
+        session.load
+        {
+            sessionExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout:kWaitExpectation)
+        { (error:Error?) in
+            
+            XCTAssertNotNil(
+                session.session,
+                "model not loaded")
+        }
+    }
 }
