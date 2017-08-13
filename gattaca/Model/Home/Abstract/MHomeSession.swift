b5f951770d89ab46bcd7b6f23780add65ff27fa9
vauxhall <zero.griffin@gmail.com>
2017-08-13 10:53:56 -0500

    Add load strategy

--- gattaca/Model/Home/Abstract/MHomeSession.swift
+++ gattaca/Model/Home/Abstract/MHomeSession.swift
@@ -2,5 +2,11 @@ import Foundation
 
 extension MHome
 {
+    //MARK: internal
     
+    func loadStrategy(controller:CHome)
+    {
+        strategy = MHomeStrategy.factoryStrategy(
+            controller:controller)
+    }
 }
