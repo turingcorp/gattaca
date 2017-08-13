3c563d2b83532e84fcb4a80cd74fb755f2bd5a80
vauxhall <zero.griffin@gmail.com>
2017-08-13 09:45:49 -0500

    Add test for loading session

--- UnitTests/Controller/Home/TCHome.swift
+++ UnitTests/Controller/Home/TCHome.swift
@@ -1,10 +1,37 @@
 import XCTest
+@testable import gattaca
 
 class TCHome:XCTestCase
 {
+    private var session:MSession?
+    private var controller:CHome?
+    private let kWaitSession:UInt32 = 3
     
     override func setUp()
     {
         super.setUp()
+        
+        let session:MSession = MSession()
+        self.session = session
+        controller = CHome(session:session)
+    }
+    
+    func testSessionLoads()
+    {
+        sleep(kWaitSession)
+        
+        guard
+            
+            let status:MSession.Status = session?.status
+            
+        else
+        {
+            return
+        }
+        
+        XCTAssertEqual(
+            status,
+            MSession.Status.loaded,
+            "session is not being loaded")
     }
 }
