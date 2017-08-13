109c1c8167b932dc658e48852a51a5defc0c93dc
vauxhall <zero.griffin@gmail.com>
2017-08-13 11:08:24 -0500

    If new load session

--- gattaca/Model/Home/Strategy/MHomeStrategyNew.swift
+++ gattaca/Model/Home/Strategy/MHomeStrategyNew.swift
@@ -2,5 +2,12 @@ import Foundation
 
 class MHomeStrategyNew:MHomeStrategy
 {
-    
+    override func nextStep()
+    {
+        controller.model.session.load
+        { [weak self] in
+            
+            self?.lastStep()
+        }
+    }
 }
