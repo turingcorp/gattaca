791496604127958ed0c8dd1aaa23ceb4df7c1d5a
vauxhall <zero.griffin@gmail.com>
2017-08-13 11:06:11 -0500

    If loading, delay and retry

--- gattaca/Model/Home/Strategy/MHomeStrategyLoading.swift
+++ gattaca/Model/Home/Strategy/MHomeStrategyLoading.swift
@@ -2,5 +2,24 @@ import Foundation
 
 class MHomeStrategyLoading:MHomeStrategy
 {
+    private let kDelay:TimeInterval = 5
     
+    override func nextStep()
+    {
+        DispatchQueue.global(qos:DispatchQoS.QoSClass.background).asyncAfter(
+            deadline:DispatchTime.now())
+        { [weak self] in
+            
+            guard
+                
+                let controller:CHome = self?.controller
+            
+            else
+            {
+                return
+            }
+            
+            self?.controller.model.loadStrategy(controller:controller)
+        }
+    }
 }
