ef0b601b82474e7b820ac10a107d9e41a3db1566
vauxhall <zero.griffin@gmail.com>
2017-08-14 11:06:15 -0500

    Factory root

--- gattaca/Model/Firebase/Database/Abstract/MFirebaseDManagerFactory.swift
+++ gattaca/Model/Firebase/Database/Abstract/MFirebaseDManagerFactory.swift
@@ -2,5 +2,34 @@ import Foundation
 
 extension MFirebaseDManager
 {
+    private static let kSlash:String = "/"
+    private static let kEmptyString:String = ""
     
+    class func factoryRoot(bundle:Bundle?) -> String
+    {
+        let useBundle:Bundle
+        
+        if let bundle:Bundle = bundle
+        {
+            useBundle = bundle
+        }
+        else
+        {
+            useBundle = Bundle.main
+        }
+        
+        guard
+        
+            let path:String = useBundle.bundleIdentifier
+        
+        else
+        {
+            return kEmptyString
+        }
+        
+        var fullPath:String = path
+        fullPath.append(kSlash)
+        
+        return fullPath
+    }
 }
