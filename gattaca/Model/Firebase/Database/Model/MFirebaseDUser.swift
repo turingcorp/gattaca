b9aeef6def362b22593c8e276755db9a7edd0fbb
vauxhall <zero.griffin@gmail.com>
2017-08-14 10:43:52 -0500

    Update model

--- gattaca/Model/Firebase/Database/Model/MFirebaseDUser.swift
+++ gattaca/Model/Firebase/Database/Model/MFirebaseDUser.swift
@@ -4,27 +4,11 @@ class MFirebaseDUser:MFirebaseDProtocol
 {
     private let kIdentifier:String = "user"
     
-    static var parent:MFirebaseDProtocol.Type?
-    {
-        get
-        {
-            return nil
-        }
-    }
-    
     var identifier:String
     {
         get
         {
             return kIdentifier
-        }
-    }
-    
-    var json:Any?
-    {
-        get
-        {
-            return nil
         }
     }
     
