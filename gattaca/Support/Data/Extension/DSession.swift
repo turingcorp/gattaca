dab83d1f6a3c1f60070f49b664e3a632bbcb4d95
vauxhall <zero.griffin@gmail.com>
2017-08-14 12:06:17 -0500

    Add property status

--- gattaca/Support/Data/Extension/DSession.swift
+++ gattaca/Support/Data/Extension/DSession.swift
@@ -2,4 +2,18 @@ import Foundation
 
 extension DSession
 {
+    var status:DSession.Status
+    {
+        get
+        {
+            let status:DSession.Status = DSession.Status(rawValue:rawStatus)!
+            
+            return status
+        }
+        
+        set
+        {
+            rawStatus = newValue.rawValue
+        }
+    }
 }
