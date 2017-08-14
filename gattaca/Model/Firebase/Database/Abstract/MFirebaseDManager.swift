eb2fee38410778af2571aacd397e45addc5cc106
vauxhall <zero.griffin@gmail.com>
2017-08-14 11:08:08 -0500

    Add root property

--- gattaca/Model/Firebase/Database/Abstract/MFirebaseDManager.swift
+++ gattaca/Model/Firebase/Database/Abstract/MFirebaseDManager.swift
@@ -1,6 +1,14 @@
 import Foundation
+import FirebaseDatabase
 
 class MFirebaseDManager
 {
+    let reference:DatabaseReference
+    let root:String
     
+    init(bundle:Bundle?)
+    {
+        reference = Database.database().reference()
+        root = MFirebaseDManager.factoryRoot(bundle:bundle)
+    }
 }
