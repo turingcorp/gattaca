08feaca042df5d6539bd844ad67b1129e33899b5
vauxhall <zero.griffin@gmail.com>
2017-08-14 10:19:37 -0500

    Implement default properties

--- gattaca/Model/Firebase/Database/Abstract/MFirebaseDProtocolExtension.swift
+++ gattaca/Model/Firebase/Database/Abstract/MFirebaseDProtocolExtension.swift
@@ -2,5 +2,38 @@ import Foundation
 
 extension MFirebaseDProtocol
 {
+    var parent:MFirebaseDProtocol?
+    {
+        get
+        {
+            return nil
+        }
+    }
     
+    var path:String
+    {
+        get
+        {
+            var modelPath:String = ""
+            
+            if let parent:MFirebaseDProtocol = self.parent
+            {
+                let parentPath:String = parent.path
+                modelPath.append(parentPath)
+                modelPath.append("/")
+            }
+            
+            modelPath.append(identifier)
+            
+            return modelPath
+        }
+    }
+    
+    var json:Any?
+    {
+        get
+        {
+            return nil
+        }
+    }
 }
