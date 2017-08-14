2b5c9e8feab14ea46774e12a473f809e159a89d7
vauxhall <zero.griffin@gmail.com>
2017-08-14 05:24:24 -0500

    Test create data model

--- UnitTests/Support/Data/Manager/TDManager.swift
+++ UnitTests/Support/Data/Manager/TDManager.swift
@@ -1,8 +1,11 @@
 import XCTest
+import CoreData
 @testable import gattaca
 
 class TDManager:XCTestCase
 {
+    private let kWaitExpectation:TimeInterval = 2
+    
     func testInit()
     {
         let manager:DManager? = DManager()
@@ -10,5 +13,30 @@ class TDManager:XCTestCase
         XCTAssertNotNil(
             manager,
             "failed to instance data manager")
+    }
+    
+    func testCreate()
+    {
+        guard
+        
+            let manager:DManager = DManager()
+        
+        else
+        {
+            return
+        }
+        
+        let createExpectation:XCTestExpectation = expectation(
+            description:"core data create model")
+        
+        manager.create(entity:DSession.self)
+        { (data:NSManagedObject) in
+            
+            createExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout:kWaitExpectation)
+        { (error:Error?) in
+        }
     }
 }
