26579b28402bfed210262a0130fcd1c7eb65b88e
vauxhall <zero.griffin@gmail.com>
2017-08-14 09:47:07 -0500

    Implement protocol

--- gattaca/Model/Firebase/Database/Model/MFirebaseDUser.swift
+++ gattaca/Model/Firebase/Database/Model/MFirebaseDUser.swift
@@ -2,5 +2,33 @@ import Foundation
 
 class MFirebaseDUser:MFirebaseDProtocol
 {
+    private let kIdentifier:String = "user"
     
+    static var parent:MFirebaseDProtocol.Type?
+    {
+        get
+        {
+            return nil
+        }
+    }
+    
+    var identifier:String
+    {
+        get
+        {
+            return kIdentifier
+        }
+    }
+    
+    var json:Any?
+    {
+        get
+        {
+            return nil
+        }
+    }
+    
+    required init?(snapshot:Any)
+    {
+    }
 }
