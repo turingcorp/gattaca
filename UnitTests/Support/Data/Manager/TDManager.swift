88e1bba506e7569092da4330d5da2aa728528ff7
vauxhall <zero.griffin@gmail.com>
2017-08-14 05:15:15 -0500

    Test init data manager

--- UnitTests/Support/Data/Manager/TDManager.swift
+++ UnitTests/Support/Data/Manager/TDManager.swift
@@ -1,5 +1,14 @@
 import XCTest
+@testable import gattaca
 
 class TDManager:XCTestCase
 {
+    func testInit()
+    {
+        let manager:DManager? = DManager()
+        
+        XCTAssertNotNil(
+            manager,
+            "failed to instance data manager")
+    }
 }
