6246fbff920e88860a88fd50cacbb0a2a9fa8902
vauxhall <zero.griffin@gmail.com>
2017-08-14 06:52:36 -0500

    Add test for session load

--- UnitTests/Model/Session/TMSessionLoader.swift
+++ UnitTests/Model/Session/TMSessionLoader.swift
@@ -41,4 +41,35 @@ class TMSessionLoader:XCTestCase
                 "failed to save changes")
         }
     }
+    
+    func testLoad()
+    {
+        let currentBundle:Bundle = Bundle(for:TDManager.self)
+        let sessionExpectation:XCTestExpectation = expectation(
+            description:"session loaded")
+        
+        guard
+            
+            let manager:DManager = DManager(bundle:currentBundle)
+            
+        else
+        {
+            return
+        }
+        
+        XCTAssertNotNil(
+            manager,
+            "failed creating manager")
+        
+        let session:MSession = MSession()
+        session.load(manager:manager)
+        { (session:DSession) in
+            
+            sessionExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout:kWaitExpectation)
+        { (error:Error?) in
+        }
+    }
 }
