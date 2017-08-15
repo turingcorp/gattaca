7353e197592072d10336308574bed30c106d4fad
vauxhall <zero.griffin@gmail.com>
2017-08-15 15:28:34 -0500

    Add test for factory actions

--- UnitTests/Model/Home/Abstract/THomeFactory.swift
+++ UnitTests/Model/Home/Abstract/THomeFactory.swift
@@ -1,5 +1,42 @@
 import XCTest
+@testable import gattaca
 
 class THomeFactory:XCTestCase
 {
+    private let kCountActions:Int = 3
+    private let kIndexHate:Int = 0
+    private let kIndexMeh:Int = 1
+    private let kIndexLike:Int = 2
+    
+    //MARK: internal
+    
+    func testFactoryActions()
+    {
+        let actions:[MHomeActionProtocol] = MHome.factoryActions()
+        let countActions:Int = actions.count
+        
+        XCTAssertEqual(
+            countActions,
+            kCountActions,
+            "incorrect number of actions")
+        
+        if countActions == kCountActions
+        {
+            let actionHate:MHomeActionHate? = actions[kIndexHate] as? MHomeActionHate
+            let actionMeh:MHomeActionMeh? = actions[kIndexMeh] as? MHomeActionMeh
+            let actionLike:MHomeActionLike? = actions[kIndexLike] as? MHomeActionLike
+            
+            XCTAssertNotNil(
+                actionHate,
+                "action hate not found")
+            
+            XCTAssertNotNil(
+                actionMeh,
+                "action meh not found")
+            
+            XCTAssertNotNil(
+                actionLike,
+                "action like not found")
+        }
+    }
 }
