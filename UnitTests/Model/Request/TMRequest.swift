a24bce80d461c25a186749dd518e654fa93d3995
vauxhall <zero.griffin@gmail.com>
2017-08-15 15:14:51 -0500

    Add tests for url map

--- UnitTests/Model/Request/TMRequest.swift
+++ UnitTests/Model/Request/TMRequest.swift
@@ -1,5 +1,41 @@
 import XCTest
+@testable import gattaca
 
 class TMRequest:XCTestCase
 {
+    //MARK: private
+    
+    private func allRootKeys() -> [String]
+    {
+        let keys:[String] = [
+            "app",
+            "gif",
+            "giphy"]
+        
+        return keys
+    }
+    
+    //MARK: internal
+    
+    func testFactoryUrlMap()
+    {
+        let urlMap:[String:AnyObject]? = MRequest.factoryUrlMap()
+        
+        XCTAssertNotNil(
+            urlMap,
+            "failed loading url map")
+    }
+    
+    func testUrlMapKeys()
+    {
+        let urlMap:[String:AnyObject]? = MRequest.factoryUrlMap()
+        let allKeys:[String] = allRootKeys()
+        
+        for key:String in allKeys
+        {
+            XCTAssertNotNil(
+                urlMap?[key],
+                "key not found in map")
+        }
+    }
 }
