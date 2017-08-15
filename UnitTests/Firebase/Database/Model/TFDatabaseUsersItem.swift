026eb6a8ebe6e1746a26471584812321e22b79b4
vauxhall <zero.griffin@gmail.com>
2017-08-15 11:38:05 -0500

    Add test for identifier

--- UnitTests/Firebase/Database/Model/TFDatabaseUsersItem.swift
+++ UnitTests/Firebase/Database/Model/TFDatabaseUsersItem.swift
@@ -1,5 +1,47 @@
 import XCTest
+@testable import gattaca
 
 class TFDatabaseUsersItem:XCTestCase
 {
+    private var user:FDatabaseUsersItem?
+    private let kIdentifier:String = "lorem ipsum"
+    
+    override func setUp()
+    {
+        super.setUp()
+        
+        let users:FDatabaseUsers = FDatabaseUsers()
+        user = FDatabaseUsersItem(
+            users:users)
+    }
+    
+    func testInit()
+    {
+        XCTAssertNotNil(
+            user,
+            "failed creating user")
+    }
+    
+    func testIdentifier()
+    {
+        guard
+        
+            let user:FDatabaseUsersItem = self.user
+        
+        else
+        {
+            return
+        }
+        
+        XCTAssertNil(
+            user.identifier,
+            "identifier should not be set")
+        
+        user.identifier = kIdentifier
+        
+        XCTAssertEqual(
+            user.identifier,
+            kIdentifier,
+            "failed setting identifier")
+    }
 }
