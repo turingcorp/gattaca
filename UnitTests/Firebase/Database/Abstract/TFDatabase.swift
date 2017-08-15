93afb5a686ba14cbdbe18f3222990e4295663757
vauxhall <zero.griffin@gmail.com>
2017-08-15 06:06:28 -0500

    Add test for create

--- UnitTests/Firebase/Database/Abstract/TFDatabase.swift
+++ UnitTests/Firebase/Database/Abstract/TFDatabase.swift
@@ -1,5 +1,39 @@
 import XCTest
+@testable import gattaca
 
 class TFDatabase:XCTestCase
 {
+    private var database:FDatabase?
+    private let kWaitExpectation:TimeInterval = 15
+    
+    override func setUp()
+    {
+        super.setUp()
+        
+        database = FDatabase()
+    }
+    
+    func testCreate()
+    {
+        let users:FDatabaseUsers = FDatabaseUsers()
+        let user:FDatabaseUsersItem = FDatabaseUsersItem(
+            users:users)
+        
+        guard
+            
+            let data:Any = user.json
+        
+        else
+        {
+            return
+        }
+        
+        let userId:String? = database?.create(
+            parent:users,
+            data:data)
+        
+        XCTAssertNotNil(
+            userId,
+            "error creating model")
+    }
 }
