afc313bc90f0e5ebefc9e4af2fcc606f51fd14fa
vauxhall <zero.griffin@gmail.com>
2017-08-15 06:25:20 -0500

    Add test for load

--- UnitTests/Firebase/Database/Abstract/TFDatabase.swift
+++ UnitTests/Firebase/Database/Abstract/TFDatabase.swift
@@ -36,4 +36,49 @@ class TFDatabase:XCTestCase
             userId,
             "error creating model")
     }
+    
+    func testLoad()
+    {
+        var firebaseUser:FDatabaseUsersItem?
+        let users:FDatabaseUsers = FDatabaseUsers()
+        let user:FDatabaseUsersItem = FDatabaseUsersItem(
+            users:users)
+        
+        guard
+            
+            let data:Any = user.json
+            
+        else
+        {
+            return
+        }
+        
+        let userId:String? = database?.create(
+            parent:users,
+            data:data)
+        
+        let loadExpectation:XCTestExpectation = expectation(
+            description:"load model")
+        
+        database?.load(
+            parent:users,
+            identifier:userId)
+        { (loadedUser:FDatabaseUsersItem?) in
+            
+            firebaseUser = loadedUser
+            loadExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout:kWaitExpectation)
+        { (error:Error?) in
+            
+            XCTAssertNotNil(
+                firebaseUser,
+                "failed loading user")
+            
+            XCTAssertNotNil(
+                firebaseUser?.identifier,
+                "user model has no id")
+        }
+    }
 }
