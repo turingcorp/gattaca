72f10d7d394a6671962c08d7e497d517934b406c
vauxhall <zero.griffin@gmail.com>
2017-08-15 03:26:33 -0500

    Add setup

--- UnitTests/Model/Session/TMSessionFirebase.swift
+++ UnitTests/Model/Session/TMSessionFirebase.swift
@@ -1,6 +1,23 @@
 import XCTest
+@testable import gattaca
 
 class TMSessionFirebase:XCTestCase
 {
+    private var manager:DManager?
+    private var session:DSession?
+    private let kWaitExpectation:TimeInterval = 15
     
+    override func setUp()
+    {
+        super.setUp()
+        
+        let currentBundle:Bundle = Bundle(for:TDManager.self)
+        
+        manager = DManager(bundle:currentBundle)
+        manager?.create
+        { [weak self] (session:DSession) in
+            
+            self?.session = session
+        }
+    }
 }
