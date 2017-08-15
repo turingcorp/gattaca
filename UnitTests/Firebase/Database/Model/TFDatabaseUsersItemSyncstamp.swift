3da3b52a75bc1dc4d1c99e601255ee38156f7f49
vauxhall <zero.griffin@gmail.com>
2017-08-15 13:32:10 -0500

    Add test for init no id

--- UnitTests/Firebase/Database/Model/TFDatabaseUsersItemSyncstamp.swift
+++ UnitTests/Firebase/Database/Model/TFDatabaseUsersItemSyncstamp.swift
@@ -1,5 +1,18 @@
 import XCTest
+@testable import gattaca
 
 class TFDatabaseUsersItemSyncstamp:XCTestCase
 {
+    func testInitNoIdentifier()
+    {
+        let users:FDatabaseUsers = FDatabaseUsers()
+        let user:FDatabaseUsersItem = FDatabaseUsersItem(
+            users:users)
+        let syncstamp:FDatabaseUsersItemSyncstamp? = FDatabaseUsersItemSyncstamp(
+            user:user)
+        
+        XCTAssertNil(
+            syncstamp,
+            "if user has no id there should be no syncstamp")
+    }
 }
