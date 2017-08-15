5e5ef229fc97f4676dacb51e75178445405173c3
vauxhall <zero.griffin@gmail.com>
2017-08-15 15:38:22 -0500

    Add test for status new

--- UnitTests/Model/Home/Strategy/TMHomeStrategyFactory.swift
+++ UnitTests/Model/Home/Strategy/TMHomeStrategyFactory.swift
@@ -1,5 +1,53 @@
 import XCTest
+@testable import gattaca
 
 class TMHomeStrategyFactory:XCTestCase
 {
+    private var controller:CHome?
+    private var session:MSession?
+    
+    override func setUp()
+    {
+        super.setUp()
+        
+        let session:MSession = MSession()
+        self.session = session
+        
+        controller = CHome(session:session)
+    }
+    
+    //MARK: internal
+    
+    func testInit()
+    {
+        XCTAssertNotNil(
+            session,
+            "failed to create session")
+        
+        XCTAssertNotNil(
+            controller,
+            "failed to create controller")
+    }
+    
+    func testFactoryStrategyNew()
+    {
+        guard
+            
+            let controller:CHome = self.controller
+        
+        else
+        {
+            return
+        }
+        
+        session?.status = MSession.Status.new
+        
+        let strategy:MHomeStrategy? = MHomeStrategy.factoryStrategy(
+            controller:controller)
+        let strategyNew:MHomeStrategyNew? = strategy as? MHomeStrategyNew
+        
+        XCTAssertNotNil(
+            strategyNew,
+            "failed loading strategy for status new")
+    }
 }
