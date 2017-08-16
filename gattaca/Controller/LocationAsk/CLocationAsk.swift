bc03d16f999b6f7a3275dae6ee05b65b8736726d
vauxhall <zero.griffin@gmail.com>
2017-08-16 06:14:39 -0500

    Use status bar light

--- gattaca/Controller/LocationAsk/CLocationAsk.swift
+++ gattaca/Controller/LocationAsk/CLocationAsk.swift
@@ -1,6 +1,9 @@
-import Foundation
+import UIKit
 
 class CLocationAsk:Controller<VLocationAsk, MLocationAsk>
 {
-    
+    override var preferredStatusBarStyle:UIStatusBarStyle
+    {
+        return UIStatusBarStyle.lightContent
+    }
 }
