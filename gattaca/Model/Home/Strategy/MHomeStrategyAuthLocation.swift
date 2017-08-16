809661f92da4075da543c379f351ef3ee5b6f243
vauxhall <zero.griffin@gmail.com>
2017-08-16 04:41:11 -0500

    Create strategy for auth location

--- gattaca/Model/Home/Strategy/MHomeStrategyAuthLocation.swift
+++ gattaca/Model/Home/Strategy/MHomeStrategyAuthLocation.swift
@@ -1,8 +1,18 @@
 import Foundation
+import CoreLocation
 
 class MHomeStrategyAuthLocation:MHomeStrategy
 {
     override func nextStep()
     {
+        let status:CLAuthorizationStatus = CLLocationManager.authorizationStatus()
+        authStatus(status:status)
+    }
+    
+    //MARK: internal
+    
+    func authStatus(status:CLAuthorizationStatus)
+    {
+        
     }
 }
