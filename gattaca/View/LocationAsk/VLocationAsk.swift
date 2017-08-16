64077e5709a4720b9b20f9e5401756345e15f59b
vauxhall <zero.griffin@gmail.com>
2017-08-16 06:14:01 -0500

    Add gradient to view

--- gattaca/View/LocationAsk/VLocationAsk.swift
+++ gattaca/View/LocationAsk/VLocationAsk.swift
@@ -2,5 +2,40 @@ import UIKit
 
 class VLocationAsk:ViewMain
 {
+    required init(controller:UIViewController)
+    {
+        super.init(controller:controller)
+        backgroundColor = UIColor.clear
+        
+        guard
+        
+            let controller:CLocationAsk = controller as? CLocationAsk
+        
+        else
+        {
+            return
+        }
+        
+        factoryViews(controller:controller)
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: private
+    
+    private func factoryViews(controller:CLocationAsk)
+    {
+        let viewGradient:VGradient = VGradient.vertical(
+            colourTop:UIColor.colourGradientLight,
+            colourBottom:UIColor.colourGradientDark)
+        
+        addSubview(viewGradient)
+        
+        NSLayoutConstraint.equals(
+            view:viewGradient,
+            toView:self)
+    }
 }
