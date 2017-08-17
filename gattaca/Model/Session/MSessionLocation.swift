fc841f083ef8f1f73a9077c8ac30c1fd1ecca153
vauxhall <zero.griffin@gmail.com>
2017-08-17 14:27:08 -0500

    Remove user from country

--- gattaca/Model/Session/MSessionLocation.swift
+++ gattaca/Model/Session/MSessionLocation.swift
@@ -2,10 +2,44 @@ import Foundation
 
 extension MSession
 {
+    //MARK: private
+    
+    private func removeCountryUser(
+        country:String,
+        userId:String)
+    {
+        let countries:FDatabaseCountries = FDatabaseCountries()
+        let country:FDatabaseCountriesItem = FDatabaseCountriesItem(
+            parent:countries,
+            identifier:country)
+        let user:FDatabaseCountriesItemUser = FDatabaseCountriesItemUser(
+            parent:country,
+            identifier:userId)
+        
+        let firebase:FDatabase = FDatabase()
+        firebase.remove(model:user)
+    }
+    
     //MARK: internal
     
     func clearDifferentLocations(country:String)
     {
+        guard
+        
+            let storedCountry:String = session?.country,
+            let userId:String = session?.userId
+        
+        else
+        {
+            return
+        }
+        
+        if storedCountry != country
+        {
+            removeCountryUser(
+                country:country,
+                userId:userId)
+        }
     }
     
     func syncNewLocation(
