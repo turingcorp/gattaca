5d1962a32abebf66646cbbd70086963831f1eb92
vauxhall <zero.griffin@gmail.com>
2017-08-17 09:42:46 -0500

    Start updating location

--- gattaca/Model/Home/Strategy/MHomeStrategyWaitingLocation.swift
+++ gattaca/Model/Home/Strategy/MHomeStrategyWaitingLocation.swift
@@ -2,6 +2,14 @@ import Foundation
 
 class MHomeStrategyWaitingLocation:MHomeStrategy
 {
+    private var delegate:MHomeStrategyWaitingLocationDelegate?
+    
+    deinit
+    {
+        delegate?.locationManager.stopUpdatingLocation()
+        delegate?.locationManager.delegate = nil
+    }
+    
     override func nextStep()
     {
         super.nextStep()
--- gattaca/Model/Home/Strategy/MHomeStrategyWaitingLocationDelegate.swift
+++ gattaca/Model/Home/Strategy/MHomeStrategyWaitingLocationDelegate.swift
@@ -3,5 +3,63 @@ import CoreLocation
 
 class MHomeStrategyWaitingLocationDelegate:NSObject, CLLocationManagerDelegate
 {
+    let locationManager:CLLocationManager
+    private weak var controller:CHome?
+    private let kDistanceFilter:CLLocationDistance = 10
+    private let kDistanceAccuracy:CLLocationDistance = 100
     
+    init(controller:CHome)
+    {
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.distanceFilter = kDistanceFilter
+        
+        super.init()
+        
+        self.controller = controller
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+    }
+    
+    deinit
+    {
+        locationManager.stopUpdatingLocation()
+    }
+    
+    //MARK: locationManagerDelegate
+    
+    func locationManager(
+        _ manager:CLLocationManager,
+        didFailWithError error:Error)
+    {
+        locationManager.stopUpdatingLocation()
+        
+        VAlert.messageFail(message:error.localizedDescription)
+    }
+    
+    func locationManager(
+        _ manager:CLLocationManager,
+        didUpdateLocations locations:[CLLocation])
+    {
+        guard
+            
+            let currentLocation:CLLocation = locations.last
+            
+        else
+        {
+            return
+        }
+        
+        let horizontalAccuracy:CLLocationAccuracy = currentLocation.horizontalAccuracy
+        
+        if horizontalAccuracy <= kDistanceAccuracy
+        {
+            locationManager.stopUpdatingLocation()
+            
+            DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
+            { [weak self] in
+                
+            }
+        }
+    }
 }
