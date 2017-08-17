9d3059fc473ed4f4ec621dbbf9a00a8f5845b843
vauxhall <zero.griffin@gmail.com>
2017-08-17 06:05:58 -0500

    Add waiting location to factory

--- gattaca/Model/Home/Strategy/MHomeStrategyFactory.swift
+++ gattaca/Model/Home/Strategy/MHomeStrategyFactory.swift
@@ -11,6 +11,7 @@ extension MHomeStrategy
             MSession.Status.loading:MHomeStrategyLoading.self,
             MSession.Status.sync:MHomeStrategySync.self,
             MSession.Status.authLocation:MHomeStrategyAuthLocation.self,
+            MSession.Status.waitingLocation:MHomeStrategyWaitingLocation.self,
             MSession.Status.loaded:MHomeStrategyLoaded.self]
         
         return map
