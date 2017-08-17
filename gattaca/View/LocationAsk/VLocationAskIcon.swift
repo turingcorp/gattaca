fba45ddf14be5b9b796ed942053be1434982d6c8
vauxhall <zero.griffin@gmail.com>
2017-08-17 03:58:23 -0500

    Layout view icon

--- gattaca/View/LocationAsk/VLocationAskIcon.swift
+++ gattaca/View/LocationAsk/VLocationAskIcon.swift
@@ -6,6 +6,32 @@ class VLocationAskIcon:
     required init(controller:CLocationAsk)
     {
         super.init(controller:controller)
+        isUserInteractionEnabled = false
+        
+        let background:UIImageView = UIImageView()
+        background.isUserInteractionEnabled = false
+        background.translatesAutoresizingMaskIntoConstraints = false
+        background.clipsToBounds = true
+        background.contentMode = UIViewContentMode.center
+        background.image = #imageLiteral(resourceName: "assetGenericCircle")
+        
+        let icon:UIImageView = UIImageView()
+        icon.isUserInteractionEnabled = false
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        icon.clipsToBounds = true
+        icon.contentMode = UIViewContentMode.center
+        icon.image = #imageLiteral(resourceName: "assetGenericLocation")
+        
+        addSubview(background)
+        addSubview(icon)
+        
+        NSLayoutConstraint.equals(
+            view:background,
+            toView:self)
+        
+        NSLayoutConstraint.equals(
+            view:icon,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
