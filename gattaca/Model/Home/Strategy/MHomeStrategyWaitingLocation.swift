4ae7d2a33b6cf628e616f4b826346002c56b5437
vauxhall <zero.griffin@gmail.com>
2017-08-17 14:47:17 -0500

    Call delegate on main thread

--- gattaca/Model/Home/Strategy/MHomeStrategyWaitingLocation.swift
+++ gattaca/Model/Home/Strategy/MHomeStrategyWaitingLocation.swift
@@ -14,10 +14,21 @@ class MHomeStrategyWaitingLocation:MHomeStrategy
     {
         super.nextStep()
         
+        DispatchQueue.main.async
+        { [weak self] in
+            
+            self?.factoryDelegate()
+        }
+    }
+    
+    //MARK: private
+    
+    private func factoryDelegate()
+    {
         guard
             
             let controller:CHome = self.controller
-        
+            
         else
         {
             return
