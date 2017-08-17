1e22dbfbee23192f4d37d0ee3c1e3e5464477253
vauxhall <zero.griffin@gmail.com>
2017-08-17 05:51:48 -0500

    Listen for became active

--- gattaca/Controller/LocationDenied/CLocationDenied.swift
+++ gattaca/Controller/LocationDenied/CLocationDenied.swift
@@ -2,9 +2,36 @@ import UIKit
 
 class CLocationDenied:Controller<VLocationDenied, MLocationDenied>
 {
+    deinit
+    {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
     override var preferredStatusBarStyle:UIStatusBarStyle
     {
         return UIStatusBarStyle.lightContent
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector:#selector(notifiedBecameActive(sender:)),
+            name:NSNotification.Name.UIApplicationDidBecomeActive,
+            object:nil)
+    }
+    
+    //MARK: notified
+    
+    func notifiedBecameActive(sender notification:Notification)
+    {
+        DispatchQueue.main.async
+        { [weak self] in
+            
+            self?.back()
+        }
     }
     
     //MARK: private
