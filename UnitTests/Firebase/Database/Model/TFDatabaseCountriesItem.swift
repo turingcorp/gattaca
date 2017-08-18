c957ae532184b66c989cf8336afd85d166373135
vauxhall <zero.griffin@gmail.com>
2017-08-18 04:49:40 -0500

    Add setup to test

--- UnitTests/Firebase/Database/Model/TFDatabaseCountriesItem.swift
+++ UnitTests/Firebase/Database/Model/TFDatabaseCountriesItem.swift
@@ -1,5 +1,18 @@
 import XCTest
+@testable import gattaca
 
 class TFDatabaseCountriesItem:XCTestCase
 {
+    private var country:FDatabaseCountriesItem?
+    private let kCountry:String = "banana republic"
+    
+    override func setUp()
+    {
+        super.setUp()
+        
+        let countries:FDatabaseCountries = FDatabaseCountries()
+        country = FDatabaseCountriesItem(
+            countries:countries,
+            identifier:kCountry)
+    }
 }
