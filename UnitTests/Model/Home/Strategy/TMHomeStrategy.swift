b77cbedd760253115f009a79417b3210c9e48f4d
vauxhall <zero.griffin@gmail.com>
2017-08-18 11:47:41 -0500

    Test delay and refresh

--- UnitTests/Model/Home/Strategy/TMHomeStrategy.swift
+++ UnitTests/Model/Home/Strategy/TMHomeStrategy.swift
@@ -6,6 +6,8 @@ class TMHomeStrategy:XCTestCase
     private var session:MSession?
     private var controller:CHome?
     private var strategy:MHomeStrategy?
+    private let kAsyncWait:TimeInterval = 6
+    private let kWaitExpectation:TimeInterval = 7
     
     override func setUp()
     {
@@ -50,5 +52,48 @@ class TMHomeStrategy:XCTestCase
         XCTAssertNotNil(
             newStrategy,
             "failed refreshing strategy")
+    }
+    
+    func testDelayAndRefresh()
+    {
+        var newStrategy:MHomeStrategyReady?
+        
+        guard
+            
+            let session:MSession = self.session,
+            let strategy:MHomeStrategy = self.strategy
+            
+        else
+        {
+            return
+        }
+        
+        let refreshExpectation:XCTestExpectation = expectation(
+            description:"refresh strategy")
+        
+        session.status = MSession.Status.ready
+        strategy.delayAndRefresh()
+        
+        DispatchQueue.main.asyncAfter(
+            deadline:DispatchTime.now() + kAsyncWait)
+        { [weak self] in
+            
+            refreshExpectation.fulfill()
+            newStrategy = self?.controller?.model.strategy as? MHomeStrategyReady
+        }
+        
+        let beforeDelayStrategy:MHomeStrategyReady? = controller?.model.strategy as? MHomeStrategyReady
+        
+        XCTAssertNil(
+            beforeDelayStrategy,
+            "refresh strategy didn't delay")
+        
+        waitForExpectations(timeout:kWaitExpectation)
+        { (error:Error?) in
+            
+            XCTAssertNotNil(
+                newStrategy,
+                "failed delay refreshing strategy")
+        }
     }
 }
