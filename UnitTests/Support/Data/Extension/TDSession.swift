dc67aabc888a93dc9393c573b22fe78c9bab4c85
vauxhall <zero.griffin@gmail.com>
2017-08-18 10:09:18 -0500

    Test initial values async

--- UnitTests/Support/Data/Extension/TDSession.swift
+++ UnitTests/Support/Data/Extension/TDSession.swift
@@ -3,15 +3,53 @@ import XCTest
 
 class TDSession:XCTestCase
 {
+    private let kWaitExpectation:TimeInterval = 2
+    
     func testInitialValues()
     {
+        var session:DSession?
         let timestamp:TimeInterval = Date().timeIntervalSince1970
-        let session:DSession = DSession()
-        session.initialValues()
         
-        XCTAssertGreaterThanOrEqual(
-            session.created,
-            timestamp,
-            "failed assigning initial values")
+        guard
+            
+            let database:Database = Database(bundle:nil)
+        
+        else
+        {
+            return
+        }
+        
+        let createExpectation:XCTestExpectation = expectation(
+            description:"core data create model")
+        
+        database.create
+        { (model:DSession) in
+            
+            model.initialValues()
+            session = model
+            createExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout:kWaitExpectation)
+        { (error:Error?) in
+            
+            XCTAssertNotNil(
+                session,
+                "failed creating session")
+            
+            guard
+                
+                let session:DSession = session
+            
+            else
+            {
+                return
+            }
+            
+            XCTAssertGreaterThanOrEqual(
+                session.created,
+                timestamp,
+                "failed assigning initial values")
+        }
     }
 }
