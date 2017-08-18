489c65f7c454e85a73615efebe221205858e707e
vauxhall <zero.griffin@gmail.com>
2017-08-18 12:10:20 -0500

    Test auth status

--- UnitTests/Model/Home/Strategy/TMHomeStrategyAuthLocation.swift
+++ UnitTests/Model/Home/Strategy/TMHomeStrategyAuthLocation.swift
@@ -1,4 +1,5 @@
 import XCTest
+import CoreLocation
 @testable import gattaca
 
 class TMHomeStrategyAuthLocation:XCTestCase
@@ -6,5 +7,14 @@ class TMHomeStrategyAuthLocation:XCTestCase
     func testAuthStatus()
     {
         let session:MSession = MSession()
+        let controller:CHome = CHome(session:session)
+        let strategy:MHomeStrategyAuthLocation = MHomeStrategyAuthLocation(
+            controller:controller)
+        
+        strategy.authStatus(status:CLAuthorizationStatus.notDetermined)
+        
+        XCTAssertNotNil(
+            strategy.strategy,
+            "failed loading strategy")
     }
 }
