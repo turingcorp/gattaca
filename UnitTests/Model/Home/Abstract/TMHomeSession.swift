d55de0419e94a30d386b72f992dd3d27c566366c
vauxhall <zero.griffin@gmail.com>
2017-08-18 10:34:39 -0500

    Test load strategy

--- UnitTests/Model/Home/Abstract/TMHomeSession.swift
+++ UnitTests/Model/Home/Abstract/TMHomeSession.swift
@@ -1,6 +1,39 @@
 import XCTest
+@testable import gattaca
 
 class TMHomeSession:XCTestCase
 {
+    private var session:MSession?
+    private var controller:CHome?
     
+    override func setUp()
+    {
+        super.setUp()
+        
+        let session:MSession = MSession(status:MSession.Status.new)
+        self.session = session
+        controller = CHome(session:session)
+    }
+    
+    func testLoadStrategy()
+    {
+        guard
+        
+            let controller:CHome = self.controller
+        
+        else
+        {
+            return
+        }
+        
+        XCTAssertNil(
+            controller.model.strategy,
+            "strategy should be nil at start")
+        
+        controller.model.loadStrategy(controller:controller)
+        
+        XCTAssertNotNil(
+            controller.model.strategy,
+            "strategy should be already loaded")
+    }
 }
