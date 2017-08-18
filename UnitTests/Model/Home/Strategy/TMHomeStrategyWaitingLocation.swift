32d26ca87347d4b709a1f9e416f1f58fa972d013
vauxhall <zero.griffin@gmail.com>
2017-08-18 12:47:39 -0500

    Test load delegate

--- UnitTests/Model/Home/Strategy/TMHomeStrategyWaitingLocation.swift
+++ UnitTests/Model/Home/Strategy/TMHomeStrategyWaitingLocation.swift
@@ -1,6 +1,39 @@
 import XCTest
+@testable import gattaca
 
 class TMHomeStrategyWaitingLocation:XCTestCase
 {
+    private let kAsyncWait:TimeInterval = 1
+    private let kWaitExpectation:TimeInterval = 2
     
+    func testDelegate()
+    {
+        let session:MSession = MSession()
+        let controller:CHome = CHome(session:session)
+        let strategy:MHomeStrategyWaitingLocation = MHomeStrategyWaitingLocation(
+            controller:controller)
+        
+        XCTAssertNil(
+            strategy.delegate,
+            "delegate should be nil")
+        
+        let delegateExpectation:XCTestExpectation = expectation(
+            description:"load delegate")
+        
+        strategy.nextStep()
+        
+        DispatchQueue.main.asyncAfter(
+            deadline:DispatchTime.now() + kAsyncWait)
+        {   
+            delegateExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout:kWaitExpectation)
+        { (error:Error?) in
+            
+            XCTAssertNotNil(
+                strategy.delegate,
+                "delegate not loaded")
+        }
+    }
 }
