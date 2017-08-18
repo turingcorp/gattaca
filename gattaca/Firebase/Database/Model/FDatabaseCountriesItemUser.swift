0071500b2615d442d39df650e1c9286dc937e866
vauxhall <zero.griffin@gmail.com>
2017-08-18 01:25:59 -0500

    Add latitude and longitude to location

--- gattaca/Firebase/Database/Model/FDatabaseCountriesItemUser.swift
+++ gattaca/Firebase/Database/Model/FDatabaseCountriesItemUser.swift
@@ -2,15 +2,24 @@ import Foundation
 
 class FDatabaseCountriesItemUser:FDatabaseProtocol
 {
+    static let kKeyLatitude:String = "latitude"
+    static let kKeyLongitude:String = "longitude"
+    
     var identifier:String?
     var parent:FDatabaseProtocol?
+    let latitude:Double
+    let longitude:Double
     
     init(
         parent:FDatabaseProtocol,
-        identifier:String)
+        identifier:String,
+        latitude:Double,
+        longitude:Double)
     {
         self.parent = parent
         self.identifier = identifier
+        self.latitude = latitude
+        self.longitude = longitude
     }
     
     required init?(json:Any)
