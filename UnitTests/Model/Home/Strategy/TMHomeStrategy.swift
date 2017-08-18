a24ead225b42c709a0f245392ab020145f8953a4
vauxhall <zero.griffin@gmail.com>
2017-08-18 11:35:41 -0500

    Test refresh strategy

--- UnitTests/Model/Home/Strategy/TMHomeStrategy.swift
+++ UnitTests/Model/Home/Strategy/TMHomeStrategy.swift
@@ -1,6 +1,54 @@
 import XCTest
+@testable import gattaca
 
 class TMHomeStrategy:XCTestCase
 {
+    private var session:MSession?
+    private var controller:CHome?
+    private var strategy:MHomeStrategy?
     
+    override func setUp()
+    {
+        super.setUp()
+        
+        let session:MSession = MSession(
+            status:MSession.Status.new)
+        self.session = session
+        
+        let controller:CHome = CHome(session:session)
+        self.controller = controller
+        
+        strategy = MHomeStrategyNew(controller:controller)
+    }
+    
+    //MARK: internal
+    
+    func testInit()
+    {
+        XCTAssertNotNil(
+            strategy,
+            "failed init strategy")
+    }
+    
+    func testRefreshStrategy()
+    {
+        guard
+        
+            let session:MSession = self.session,
+            let strategy:MHomeStrategy = self.strategy
+        
+        else
+        {
+            return
+        }
+        
+        session.status = MSession.Status.ready
+        strategy.refreshStrategy()
+        
+        let newStrategy:MHomeStrategyReady? = controller?.model.strategy as? MHomeStrategyReady
+        
+        XCTAssertNotNil(
+            newStrategy,
+            "failed refreshing strategy")
+    }
 }
