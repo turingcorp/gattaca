4c20fa4d5ba73127c30eebe32eb7f769713d5fe1
vauxhall <zero.griffin@gmail.com>
2017-08-18 09:54:19 -0500

    Test child path

--- UnitTests/Firebase/Database/Protocol/TFDatabaseProtocolExtension.swift
+++ UnitTests/Firebase/Database/Protocol/TFDatabaseProtocolExtension.swift
@@ -5,6 +5,7 @@ class TFDatabaseProtocolExtension:XCTestCase
 {
     private let kUserId:String = "lorem ipsum"
     private let kSlash:String = "/"
+    private let kCountry:String = "banana republic"
     
     //MARK: internal
     
@@ -39,5 +40,42 @@ class TFDatabaseProtocolExtension:XCTestCase
             expectedPath,
             protocolPath,
             "error generating path")
+    }
+    
+    func testChild()
+    {
+        let countries:FDatabaseCountries = FDatabaseCountries()
+        let country:FDatabaseCountriesItem = FDatabaseCountriesItem(
+            countries:countries,
+            identifier:kCountry)
+        let child:String = country.child(identifier:kUserId)
+        let childComponents:[String] = child.components(separatedBy:kSlash)
+        let countComponenets:Int = childComponents.count
+        
+        XCTAssertEqual(
+            countComponenets,
+            3,
+            "There should be 3 components on child name")
+        
+        let firstComponent:String = childComponents[0]
+        
+        XCTAssertEqual(
+            firstComponent,
+            countries.identifier,
+            "first component mismatch")
+        
+        let secondComponent:String = childComponents[1]
+        
+        XCTAssertEqual(
+            secondComponent,
+            kCountry,
+            "second component mismatch")
+        
+        let thirdComponent:String = childComponents[2]
+        
+        XCTAssertEqual(
+            thirdComponent,
+            kUserId,
+            "third component mismatch")
     }
 }
