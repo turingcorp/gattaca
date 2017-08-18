a41f12d32419d773562deaa13401b69d0418b1b3
vauxhall <zero.griffin@gmail.com>
2017-08-18 01:39:57 -0500

    Create json for location

--- gattaca/Firebase/Database/Model/FDatabaseCountriesItemUser.swift
+++ gattaca/Firebase/Database/Model/FDatabaseCountriesItemUser.swift
@@ -4,11 +4,26 @@ class FDatabaseCountriesItemUser:FDatabaseProtocol
 {
     static let kKeyLatitude:String = "latitude"
     static let kKeyLongitude:String = "longitude"
+    static let kKeySyncstamp:String = "syncstamp"
     
     var identifier:String?
     var parent:FDatabaseProtocol?
     let latitude:Double
     let longitude:Double
+    let syncstamp:TimeInterval
+    
+    var json:Any?
+    {
+        get
+        {
+            let json:[String:Any] = [
+                FDatabaseCountriesItemUser.kKeyLatitude:latitude,
+                FDatabaseCountriesItemUser.kKeyLongitude:longitude,
+                FDatabaseCountriesItemUser.kKeySyncstamp:syncstamp]
+            
+            return json
+        }
+    }
     
     init(
         parent:FDatabaseProtocol,
@@ -20,6 +35,7 @@ class FDatabaseCountriesItemUser:FDatabaseProtocol
         self.identifier = identifier
         self.latitude = latitude
         self.longitude = longitude
+        syncstamp = Date().timeIntervalSince1970
     }
     
     required init?(json:Any)
