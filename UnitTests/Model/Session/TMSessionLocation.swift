136e464e1222f2ba5c6f7a5c9069404a1554d6bd
vauxhall <zero.griffin@gmail.com>
2017-08-19 02:44:39 -0500

    Test location gets synced

--- UnitTests/Model/Session/TMSessionLocation.swift
+++ UnitTests/Model/Session/TMSessionLocation.swift
@@ -6,18 +6,31 @@ class TMSessionLocation:XCTestCase
     private let kCountry:String = "banana republic"
     private let kLatitude:Double = 1
     private let kLongitude:Double = 2
+    private let kWaitExpectation:TimeInterval = 15
     
     //MARK: internal
     
     func testSyncLocation()
     {
+        let syncExpectation:XCTestExpectation = expectation(
+            description:"sync location")
+        
         let session:MSession = MSession()
         session.syncLocation(
             latitude:kLatitude,
             longitude:kLongitude,
             country:kCountry)
         {
+            syncExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout:kWaitExpectation)
+        { (error:Error?) in
             
+            XCTAssertEqual(
+                session.status,
+                MSession.Status.ready,
+                "location not syncing")
         }
     }
 }
