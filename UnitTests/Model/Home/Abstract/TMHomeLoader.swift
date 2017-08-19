0429e1de67e2eb73ca5717e272df319cd4e101f4
vauxhall <zero.griffin@gmail.com>
2017-08-19 10:54:29 -0500

    Test async load items

--- UnitTests/Model/Home/Abstract/TMHomeLoader.swift
+++ UnitTests/Model/Home/Abstract/TMHomeLoader.swift
@@ -1,5 +1,48 @@
 import XCTest
+@testable import gattaca
 
 class TMHomeLoader:XCTestCase
 {
+    private let kWaitExpectation:TimeInterval = 20
+    
+    //MARK: internal
+    
+    func testAsyncLoadItems()
+    {
+        let session:MSession = MSession()
+        let model:MHome = MHome(session:session)
+        var items:[MHomeItem]?
+        
+        let loadExpectation:XCTestExpectation = expectation(
+            description:"load items")
+        
+        model.asyncLoadItems
+        { (loadedItems:[MHomeItem]) in
+        
+            items = loadedItems
+            loadExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout:kWaitExpectation)
+        { (error:Error?) in
+            
+            XCTAssertNotNil(
+                items,
+                "failed loading items")
+            
+            guard
+                
+                let count:Int = items?.count
+            
+            else
+            {
+                return
+            }
+            
+            XCTAssertGreaterThan(
+                count,
+                0,
+                "Zero loaded items")
+        }
+    }
 }
