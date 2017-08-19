1dd6789b2690bc13b58987dd66b34399d099358f
vauxhall <zero.griffin@gmail.com>
2017-08-19 10:50:00 -0500

    Test load items

--- UnitTests/Model/Home/Abstract/TMHome.swift
+++ UnitTests/Model/Home/Abstract/TMHome.swift
@@ -1,5 +1,38 @@
 import XCTest
+@testable import gattaca
 
 class TMHome:XCTestCase
-{   
+{
+    private let kDelay:TimeInterval = 20
+    private let kWaitExpectation:TimeInterval = 21
+    
+    //MARK: internal
+    
+    func testLoadItems()
+    {
+        let session:MSession = MSession()
+        let model:MHome = MHome(session:session)
+        
+        model.loadItems()
+        
+        let loadExpectation:XCTestExpectation = expectation(
+            description:"load items")
+        
+        DispatchQueue.main.asyncAfter(
+            deadline:DispatchTime.now())
+        {
+            loadExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout:kWaitExpectation)
+        { (error:Error?) in
+            
+            let countItems:Int = model.items.count
+            
+            XCTAssertGreaterThan(
+                countItems,
+                0,
+                "failed loading items")
+        }
+    }
 }
