2cd3263ba31c050cdc702c4f861cf94ccd797f79
vauxhall <zero.griffin@gmail.com>
2017-08-18 20:21:39 -0500

    Test for sync location

--- UnitTests/Model/Session/TMSessionLocation.swift
+++ UnitTests/Model/Session/TMSessionLocation.swift
@@ -1,5 +1,23 @@
 import XCTest
+@testable import gattaca
 
 class TMSessionLocation:XCTestCase
 {
+    private let kCountry:String = "banana republic"
+    private let kLatitude:Double = 1
+    private let kLongitude:Double = 2
+    
+    //MARK: internal
+    
+    func testSyncLocation()
+    {
+        let session:MSession = MSession()
+        session.syncLocation(
+            latitude:kLatitude,
+            longitude:kLongitude,
+            country:kCountry)
+        {
+            
+        }
+    }
 }
