3a2205d871a99c1f943ef8fc636a419ae5ab3577
vauxhall <zero.griffin@gmail.com>
2017-08-20 18:20:21 -0500

    Test load complete and map

--- UnitTests/Model/Gif/TMGifLoader.swift
+++ UnitTests/Model/Gif/TMGifLoader.swift
@@ -3,18 +3,49 @@ import XCTest
 
 class TMGifLoader:XCTestCase
 {
+    private var coreData:Database?
+    private let kIdentifier:String = "lorem ipsum"
     private let kWaitExpectation:TimeInterval = 3
+    
+    override func setUp()
+    {
+        super.setUp()
+        
+        let bundle:Bundle = Bundle(for:TMGifLoader.self)
+        coreData = Database(bundle:bundle)
+    }
+    
+    //MARK: private
+    
+    private func createGif(completion:@escaping((DGif) -> ()))
+    {
+        guard
+            
+            let coreData:Database = self.coreData
+            
+        else
+        {
+            return
+        }
+        
+        let identifier:String = kIdentifier
+        
+        coreData.create
+        { (gif:DGif) in
+            
+            gif.initialValues(identifier:identifier)
+            completion(gif)
+        }
+    }
     
     //MARK: internal
     
     func testLoad()
     {
-        let bundle:Bundle = Bundle(for:TMGifLoader.self)
-        
         guard
             
-            let coreData:Database = Database(bundle:bundle)
-        
+            let coreData:Database = self.coreData
+            
         else
         {
             return
@@ -31,6 +62,32 @@ class TMGifLoader:XCTestCase
         
         waitForExpectations(timeout:kWaitExpectation)
         { (error:Error?) in
+        }
+    }
+    
+    func testLoadCompleteAndMap()
+    {
+        let identifier:String = kIdentifier
+        let gif:MGif = MGif()
+        let loadExpectation:XCTestExpectation = expectation(
+            description:"load gifs")
+        
+        createGif
+        { (item:DGif) in
+            
+            let items:[DGif] = [item]
+            gif.loadComplete(gifs:items)
+            {
+                loadExpectation.fulfill()
+            }
+        }
+        
+        waitForExpectations(timeout:kWaitExpectation)
+        { (error:Error?) in
+            
+            XCTAssertNotNil(
+                gif.map[identifier],
+                "failed mapping item")
         }
     }
 }
