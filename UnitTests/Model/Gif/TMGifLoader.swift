c5cd6bbb7af163ded2ae4389c51ef151bfe2f3b4
vauxhall <zero.griffin@gmail.com>
2017-08-20 18:09:14 -0500

    Test load gif

--- UnitTests/Model/Gif/TMGifLoader.swift
+++ UnitTests/Model/Gif/TMGifLoader.swift
@@ -1,5 +1,36 @@
 import XCTest
+@testable import gattaca
 
 class TMGifLoader:XCTestCase
 {
+    private let kWaitExpectation:TimeInterval = 3
+    
+    //MARK: internal
+    
+    func testLoad()
+    {
+        let bundle:Bundle = Bundle(for:TMGifLoader.self)
+        
+        guard
+            
+            let coreData:Database = Database(bundle:bundle)
+        
+        else
+        {
+            return
+        }
+        
+        let loadExpectation:XCTestExpectation = expectation(
+            description:"load gifs")
+        
+        let gif:MGif = MGif()
+        gif.load(coreData:coreData)
+        {
+            loadExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout:kWaitExpectation)
+        { (error:Error?) in
+        }
+    }
 }
