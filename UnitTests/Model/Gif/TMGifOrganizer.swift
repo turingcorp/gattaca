cbe12aa8f45bce99f0d71346ae4de647d6e68fd2
vauxhall <zero.griffin@gmail.com>
2017-08-21 17:42:11 -0500

    Test purge items

--- UnitTests/Model/Gif/TMGifOrganizer.swift
+++ UnitTests/Model/Gif/TMGifOrganizer.swift
@@ -36,7 +36,7 @@ class TMGifOrganizer:XCTestCase
         }
         
         let addExpectation:XCTestExpectation = expectation(
-            description:"add item expection")
+            description:"add item")
         let identifier:String = kIdentifier
         
         coreData.create
@@ -68,7 +68,110 @@ class TMGifOrganizer:XCTestCase
             
             XCTAssertNotNil(
                 storedItem,
+                "didn't map item on add")
+        }
+    }
+    
+    func testMapItem()
+    {
+        guard
+            
+            let coreData:Database = self.coreData
+            
+        else
+        {
+            return
+        }
+        
+        let mapExpectation:XCTestExpectation = expectation(
+            description:"map item")
+        let identifier:String = kIdentifier
+        
+        coreData.create
+        { [weak self] (item:DGif) in
+            
+            item.initialValues(identifier:identifier)
+            self?.model?.gif.mapItem(item:item)
+            mapExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout:kWaitExpectation)
+        { [weak self] (error:Error?) in
+            
+            guard
+                
+                let gif:MGif = self?.model?.gif
+                
+            else
+            {
+                return
+            }
+            
+            let storedItem:DGif? = gif.map[identifier]
+            
+            XCTAssertNotNil(
+                storedItem,
                 "failed mapping item")
+        }
+    }
+    
+    func testPurgeItems()
+    {
+        guard
+            
+            let coreData:Database = self.coreData
+            
+        else
+        {
+            return
+        }
+        
+        var items:[MGiphyItem]?
+        let purgeExpectation:XCTestExpectation = expectation(
+            description:"map item")
+        let identifier:String = kIdentifier
+        let giphyItem:MGiphyItem = MGiphyItem(identifier:identifier)
+        
+        coreData.create
+        { [weak self] (item:DGif) in
+                
+            item.initialValues(identifier:identifier)
+            
+            guard
+                
+                let gif:MGif = self?.model?.gif
+            
+            else
+            {
+                return
+            }
+            
+            gif.newItem(item:item)
+            
+            items = gif.purgeItems(items:[giphyItem])
+            purgeExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout:kWaitExpectation)
+        { (error:Error?) in
+            
+            XCTAssertNotNil(
+                items,
+                "failed purging items")
+            
+            guard
+            
+                let count:Int = items?.count
+            
+            else
+            {
+                return
+            }
+            
+            XCTAssertEqual(
+                count,
+                0,
+                "failed purging items")
         }
     }
 }
