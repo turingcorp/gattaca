79e31418bd66656f9dd39884de01ba3c4adc08ea
vauxhall <zero.griffin@gmail.com>
2017-08-21 11:02:43 -0500

    Test create directory

--- UnitTests/Model/Gif/TMGifDirectory.swift
+++ UnitTests/Model/Gif/TMGifDirectory.swift
@@ -5,12 +5,60 @@ class TMGifDirectory:XCTestCase
 {
     //MARK: internal
     
-    func testCreateDirectory()
+    func testDirectory()
     {
-        let directory:URL? = MGif.gifDirectory()
+        let directory:URL? = MGif.directory()
         
         XCTAssertNotNil(
             directory,
+            "failed referencing directory")
+    }
+    
+    func testCreateDirectory()
+    {
+        let dir:URL? = MGif.createDirectory()
+        
+        XCTAssertNotNil(
+            dir,
             "failed creating directory")
+        
+        guard
+            
+            let directory:URL = dir
+        
+        else
+        {
+            return
+        }
+        
+        let resourceSets:Set<URLResourceKey> = Set<URLResourceKey>(
+            [URLResourceKey.isExcludedFromBackupKey])
+        let resourceValues:URLResourceValues
+        
+        do
+        {
+            try resourceValues = directory.resourceValues(forKeys:resourceSets)
+        }
+        catch
+        {
+            return
+        }
+        
+        XCTAssertNotNil(
+            resourceValues.isExcludedFromBackup,
+            "exclude from backup key missing")
+        
+        guard
+            
+            let shouldExclude:Bool = resourceValues.isExcludedFromBackup
+        
+        else
+        {
+            return
+        }
+        
+        XCTAssertTrue(
+            shouldExclude,
+            "directory not excluded from backup")
     }
 }
