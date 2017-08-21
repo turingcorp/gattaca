e2683c97945436a735c539b0dc1596ca4f9b153a
vauxhall <zero.griffin@gmail.com>
2017-08-21 17:06:35 -0500

    Move grouping methods to organizer

--- gattaca/Model/Gif/MGif.swift
+++ gattaca/Model/Gif/MGif.swift
@@ -32,41 +32,4 @@ class MGif
     {
         strategy = MGifStrategyDownload(model:self)
     }
-    
-    func mapItem(item:DGif)
-    {
-        guard
-            
-            let identifier:String = item.identifier
-            
-        else
-        {
-            return
-        }
-        
-        map[identifier] = item
-    }
-    
-    func newItem(item:DGif)
-    {
-        itemsNotReady.append(item)
-        mapItem(item:item)
-    }
-    
-    func purgeItems(items:[MGiphyItem]) -> [MGiphyItem]
-    {
-        var purged:[MGiphyItem] = []
-        
-        for item in items
-        {
-            let identifier:String = item.identifier
-            
-            if map[identifier] == nil
-            {
-                purged.append(item)
-            }
-        }
-        
-        return purged
-    }
 }
--- gattaca/Model/Gif/MGifOrganizer.swift
+++ gattaca/Model/Gif/MGifOrganizer.swift
@@ -2,5 +2,42 @@ import Foundation
 
 extension MGif
 {
+    //MARK: internal
     
+    func mapItem(item:DGif)
+    {
+        guard
+            
+            let identifier:String = item.identifier
+            
+        else
+        {
+            return
+        }
+        
+        map[identifier] = item
+    }
+    
+    func newItem(item:DGif)
+    {
+        itemsNotReady.append(item)
+        mapItem(item:item)
+    }
+    
+    func purgeItems(items:[MGiphyItem]) -> [MGiphyItem]
+    {
+        var purged:[MGiphyItem] = []
+        
+        for item in items
+        {
+            let identifier:String = item.identifier
+            
+            if map[identifier] == nil
+            {
+                purged.append(item)
+            }
+        }
+        
+        return purged
+    }
 }
