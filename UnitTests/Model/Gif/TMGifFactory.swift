9049ebc41cd0559822cade2ec3fdd12e43696be0
vauxhall <zero.griffin@gmail.com>
2017-08-21 11:16:30 -0500

    Test factory path

--- UnitTests/Model/Gif/TMGifFactory.swift
+++ UnitTests/Model/Gif/TMGifFactory.swift
@@ -1,6 +1,36 @@
 import XCTest
+@testable import gattaca
 
 class TMGifFactory:XCTestCase
 {
+    private let kIdentifier:String = "lorem.ipsum"
+    private let kSlash:String = "/"
     
+    //MARK: internal
+    
+    func testFactoryPath()
+    {
+        guard
+            
+            let directory:URL = MGif.directory()
+        
+        else
+        {
+            return
+        }
+        
+        let path:URL? = MGif.factoryPath(
+            identifier:kIdentifier,
+            directory:directory)
+        
+        XCTAssertNotNil(
+            path,
+            "failed factorying gif path")
+        
+        let pathString:String? = path?.path
+        
+        XCTAssertNotNil(
+            pathString,
+            "failed creating path string")
+    }
 }
