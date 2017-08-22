898ba3af25a40b3f5583d6ae2e4d78496b8efae9
vauxhall <zero.griffin@gmail.com>
2017-08-22 11:15:25 -0500

    Test init strategy

--- UnitTests/Model/Gif/Strategy/TMGifStrategyDownload.swift
+++ UnitTests/Model/Gif/Strategy/TMGifStrategyDownload.swift
@@ -1,6 +1,17 @@
 import XCTest
+@testable import gattaca
 
 class TMGifStrategyDownload:XCTestCase
 {
-
+    //MARK: internal
+    
+    func testInit()
+    {
+        let gif:MGif = MGif()
+        let strategy:MGifStrategyDownload? = MGifStrategyDownload(model:gif)
+        
+        XCTAssertNotNil(
+            strategy,
+            "failed loading strategy")
+    }
 }
