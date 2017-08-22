5c47cd061749b9ea9b27a3bf0e7b73cb62a44f6c
vauxhall <zero.griffin@gmail.com>
2017-08-22 10:44:07 -0500

    Test factory items

--- UnitTests/Model/Giphy/TMGiphyFactory.swift
+++ UnitTests/Model/Giphy/TMGiphyFactory.swift
@@ -1,6 +1,105 @@
 import XCTest
+@testable import gattaca
 
 class TMGiphyFactory:XCTestCase
 {
+    private let kTrendingName:String = "giphyTrending"
+    private let kTrendingExtension:String = "json"
+    private let kCountTrendingItems:Int = 3
     
+    //MARK: private
+    
+    private func factoryTrendingJson() -> Any?
+    {
+        let bundle:Bundle = Bundle(for:TMHomeRequest.self)
+        
+        guard
+            
+            let url:URL = bundle.url(
+                forResource:kTrendingName,
+                withExtension:kTrendingExtension)
+            
+        else
+        {
+            return nil
+        }
+        
+        let data:Data
+        
+        do
+        {
+            try data = Data(
+                contentsOf:url,
+                options:Data.ReadingOptions.uncached)
+        }
+        catch
+        {
+            return nil
+        }
+        
+        let json:Any
+        
+        do
+        {
+            try json = JSONSerialization.jsonObject(
+                with:data,
+                options:JSONSerialization.ReadingOptions.allowFragments)
+        }
+        catch
+        {
+            return nil
+        }
+        
+        return json
+    }
+    
+    //MARK: internal
+    
+    func testFactoryItems()
+    {
+        let trendingJson:Any? = factoryTrendingJson()
+        
+        XCTAssertNotNil(
+            trendingJson,
+            "failed loading json")
+        
+        guard
+        
+            let json:Any = trendingJson
+        
+        else
+        {
+            return
+        }
+        
+        let items:[MGiphyItem]? = MGiphy.factoryItems(json:json)
+        
+        XCTAssertNotNil(
+            items,
+            "failed factorying items")
+        
+        XCTAssertEqual(
+            items?.count,
+            kCountTrendingItems,
+            "incorrect number of items")
+        
+        guard
+        
+            let parsedItems:[MGiphyItem] = items
+        
+        else
+        {
+            return
+        }
+        
+        for item:MGiphyItem in parsedItems
+        {
+            let identifierLength:Int = item.identifier.characters.count
+            
+            XCTAssertGreaterThan(
+                identifierLength,
+                0,
+                "failed parsing identifier")
+        }
+    }
 }
