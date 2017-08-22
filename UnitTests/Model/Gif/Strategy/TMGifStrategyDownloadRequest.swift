713465767b3381878bf86dd622e94334f94b0792
vauxhall <zero.griffin@gmail.com>
2017-08-22 11:19:33 -0500

    Test factory url

--- UnitTests/Model/Gif/Strategy/TMGifStrategyDownloadRequest.swift
+++ UnitTests/Model/Gif/Strategy/TMGifStrategyDownloadRequest.swift
@@ -1,6 +1,31 @@
 import XCTest
+@testable import gattaca
 
 class TMGifStrategyDownloadRequest:XCTestCase
 {
+    private let kIdentifier:String = "xUNd9Cr6NMXPBO1t7y"
     
+    //MARK: internal
+    
+    func testFactoryUrl()
+    {
+        let gif:MGif = MGif()
+        
+        guard
+        
+            let strategy:MGifStrategyDownload = MGifStrategyDownload(
+                model:gif)
+        
+        else
+        {
+            return
+        }
+        
+        let url:URL? = strategy.factoryUrl(
+            identifier:kIdentifier)
+        
+        XCTAssertNotNil(
+            url,
+            "failed factorying url")
+    }
 }
