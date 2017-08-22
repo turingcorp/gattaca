401fbcf78c36f77d705926bda70f67c6fc659a4a
vauxhall <zero.griffin@gmail.com>
2017-08-22 11:08:49 -0500

    Test factory url and request

--- UnitTests/Model/Giphy/TMGiphyRandom.swift
+++ UnitTests/Model/Giphy/TMGiphyRandom.swift
@@ -1,5 +1,25 @@
 import XCTest
+@testable import gattaca
 
 class TMGiphyRandom:XCTestCase
 {
+    //MARK: internal
+    
+    func testFactoryUrl()
+    {
+        let url:URL? = MGiphy.factoryRandomUrl()
+        
+        XCTAssertNotNil(
+            url,
+            "failed factorying url")
+    }
+    
+    func testFactoryRequest()
+    {
+        let request:URLRequest? = MGiphy.factoryRandomRequest()
+        
+        XCTAssertNotNil(
+            request,
+            "failed factorying request")
+    }
 }
