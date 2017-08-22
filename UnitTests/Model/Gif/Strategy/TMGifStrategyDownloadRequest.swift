c33223a65eff0df4f3f2f001457e54b78f4f444f
vauxhall <zero.griffin@gmail.com>
2017-08-22 14:08:30 -0500

    Test downloaded data

--- UnitTests/Model/Gif/Strategy/TMGifStrategyDownloadRequest.swift
+++ UnitTests/Model/Gif/Strategy/TMGifStrategyDownloadRequest.swift
@@ -3,29 +3,115 @@ import XCTest
 
 class TMGifStrategyDownloadRequest:XCTestCase
 {
+    private var urlResponseOk:URLResponse?
+    private var urlResponseFail:URLResponse?
+    private var gif:MGif?
+    private var strategy:MGifStrategyDownload?
+    private var fileUrl:URL?
     private let kIdentifier:String = "xUNd9Cr6NMXPBO1t7y"
+    private let kFileName:String = "giphyTrending"
+    private let kFileExtension:String = "json"
+    private let kStatusCodeOk:Int = 200
+    private let kStatusCodeFail:Int = 0
     
-    //MARK: internal
-    
-    func testFactoryUrl()
+    override func setUp()
     {
+        super.setUp()
+        
         let gif:MGif = MGif()
+        self.gif = gif
+        
+        strategy = MGifStrategyDownload(
+            model:gif)
+        
+        let bundle:Bundle = Bundle(for:TMHomeRequest.self)
         
         guard
-        
-            let strategy:MGifStrategyDownload = MGifStrategyDownload(
-                model:gif)
-        
+            
+            let fileUrl:URL = bundle.url(
+                forResource:kFileName,
+                withExtension:kFileExtension)
+            
         else
         {
             return
         }
         
-        let url:URL? = strategy.factoryUrl(
+        self.fileUrl = fileUrl
+        urlResponseOk = HTTPURLResponse(
+            url:fileUrl,
+            statusCode:kStatusCodeOk,
+            httpVersion:nil,
+            headerFields:nil)
+        
+        urlResponseFail = HTTPURLResponse(
+            url:fileUrl,
+            statusCode:kStatusCodeFail,
+            httpVersion:nil,
+            headerFields:nil)
+    }
+    
+    //MARK: internal
+    
+    func testSetup()
+    {
+        XCTAssertNotNil(
+            fileUrl,
+            "failed creating url")
+        
+        XCTAssertNotNil(
+            urlResponseOk,
+            "failed creating response")
+    }
+    
+    func testFactoryUrl()
+    {
+        let url:URL? = strategy?.factoryUrl(
             identifier:kIdentifier)
         
         XCTAssertNotNil(
             url,
             "failed factorying url")
+    }
+    
+    func testDownloadedData()
+    {
+        let data:Data? = strategy?.downloadedData(
+            fileUrl:fileUrl,
+            urlResponse:urlResponseOk,
+            error:nil)
+        
+        XCTAssertNotNil(
+            data,
+            "failed creating data")
+    }
+    
+    func testDownloadedDataResponseFailed()
+    {
+        let data:Data? = strategy?.downloadedData(
+            fileUrl:fileUrl,
+            urlResponse:urlResponseFail,
+            error:nil)
+        
+        XCTAssertNil(
+            data,
+            "data should not be created")
+    }
+    
+    func testDownloadedDataError()
+    {
+        let error:Error = NSError(
+            domain:kIdentifier,
+            code:0,
+            userInfo:nil)
+        
+        let data:Data? = strategy?.downloadedData(
+            fileUrl:fileUrl,
+            urlResponse:urlResponseOk,
+            error:error)
+        
+        XCTAssertNil(
+            data,
+            "data should not be created")
     }
 }
