c896a4f3331f0cc48470bb6e7c4f2fb45d85bf56
vauxhall <zero.griffin@gmail.com>
2017-08-22 11:05:29 -0500

    Test factory url

--- UnitTests/Model/Giphy/TMGiphyTrending.swift
+++ UnitTests/Model/Giphy/TMGiphyTrending.swift
@@ -1,5 +1,23 @@
 import XCTest
+@testable import gattaca
 
 class TMGiphyTrending:XCTestCase
 {
+    private let kOffset:Int = 0
+    private let kLimit:Int = 10
+    
+    //MARK: internal
+    
+    func testFactoryUrl()
+    {
+        let url:URL? = MGiphy.factoryTrendingUrl(
+            offset:kOffset,
+            limit:kLimit)
+        
+        XCTAssertNotNil(
+            url,
+            "failed factorying url")
+    }
+    
+    
 }
