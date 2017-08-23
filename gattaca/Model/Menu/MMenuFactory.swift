2484d80b802568a95fcc4fe2bd11c936cd16e851
vauxhall <zero.griffin@gmail.com>
2017-08-23 15:54:45 -0500

    Add settings to factory

--- gattaca/Model/Menu/MMenuFactory.swift
+++ gattaca/Model/Menu/MMenuFactory.swift
@@ -4,10 +4,12 @@ extension MMenu
 {
     class func factoryItems() -> [MMenuItemProtocol]
     {
+        let itemSettings:MMenuItemSettings = MMenuItemSettings()
         let itemMatch:MMenuItemMatch = MMenuItemMatch()
         let itemHome:MMenuItemHome = MMenuItemHome()
         
         let items:[MMenuItemProtocol] = [
+            itemSettings,
             itemHome,
             itemMatch]
         
