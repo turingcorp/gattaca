68ce637bcc9b917ee14a417f2c324a4e3a62fbcd
vauxhall <zero.griffin@gmail.com>
2017-08-23 09:42:08 -0500

    Separate gesture recognizer

--- gattaca/View/Abstract/Basic/ViewParent.swift
+++ gattaca/View/Abstract/Basic/ViewParent.swift
@@ -1,14 +1,14 @@
 import UIKit
 
-class ViewParent:UIView, UIGestureRecognizerDelegate
+class ViewParent:UIView
 {
     weak var panRecognizer:UIPanGestureRecognizer!
+    var panningX:CGFloat?
+    let kMaxXPanning:CGFloat = 60
+    let kMaxXDelta:CGFloat = 210
+    let kMinXDelta:CGFloat = 30
     let kAnimationDuration:TimeInterval = 0.3
-    private weak var controller:ControllerParent!
-    private var panningX:CGFloat?
-    private let kMaxXPanning:CGFloat = 60
-    private let kMaxXDelta:CGFloat = 210
-    private let kMinXDelta:CGFloat = 30
+    private(set) weak var controller:ControllerParent!
     
     init(controller:ControllerParent)
     {
@@ -17,140 +17,11 @@ class ViewParent:UIView, UIGestureRecognizerDelegate
         backgroundColor = UIColor.white
         self.controller = controller
         
-        let panRecognizer:UIPanGestureRecognizer = UIPanGestureRecognizer(
-            target:self,
-            action:#selector(actionPanRecognized(sender:)))
-        panRecognizer.delegate = self
-        self.panRecognizer = panRecognizer
-        
-        addGestureRecognizer(panRecognizer)
+        factoryGesture()
     }
     
     required init?(coder:NSCoder)
     {
         return nil
-    }
-    
-    //MARK: selectors
-    
-    func actionPanRecognized(sender panGesture:UIPanGestureRecognizer)
-    {
-        let location:CGPoint = panGesture.location(in:self)
-        let xPos:CGFloat = location.x
-        
-        switch panGesture.state
-        {
-        case UIGestureRecognizerState.began,
-             UIGestureRecognizerState.possible:
-            
-            if xPos < kMaxXPanning
-            {
-                self.panningX = xPos
-            }
-            
-            break
-            
-        case UIGestureRecognizerState.changed:
-            
-            if let panningX:CGFloat = self.panningX
-            {
-                var deltaX:CGFloat = xPos - panningX
-                
-                if deltaX > kMaxXDelta
-                {
-                    panRecognizer.isEnabled = false
-                }
-                else
-                {
-                    if deltaX < 0
-                    {
-                        deltaX = 0
-                    }
-                    
-                    guard
-                        
-                        let topView:ViewMain = subviews.last as? ViewMain
-                        
-                    else
-                    {
-                        return
-                    }
-                    
-                    topView.layoutLeft.constant = deltaX
-                }
-            }
-            
-            break
-            
-        case UIGestureRecognizerState.cancelled,
-             UIGestureRecognizerState.ended,
-             UIGestureRecognizerState.failed:
-            
-            if let panningX:CGFloat = self.panningX
-            {
-                let deltaX:CGFloat = xPos - panningX
-                
-                if deltaX > kMinXDelta
-                {
-                    gesturePop()
-                }
-                else
-                {
-                    gestureRestore()
-                }
-            }
-            
-            panningX = nil
-            
-            break
-        }
-    }
-    
-    //MARK: private
-    
-    private func gesturePop()
-    {
-        panRecognizer.isEnabled = true
-        controller.pop(horizontal:ControllerParent.Horizontal.right)
-    }
-    
-    private func gestureRestore()
-    {
-        panRecognizer.isEnabled = true
-        
-        guard
-            
-            let topView:ViewProtocol = subviews.last as? ViewProtocol
-            
-        else
-        {
-            return
-        }
-        
-        topView.layoutLeft.constant = 0
-        
-        UIView.animate(withDuration:kAnimationDuration)
-        {
-            self.layoutIfNeeded()
-        }
-    }
-    
-    //MARK: gestureRecognizer delegate
-    
-    override func gestureRecognizerShouldBegin(
-        _ gestureRecognizer:UIGestureRecognizer) -> Bool
-    {
-        guard
-        
-            let view:ViewProtocol = subviews.last as? ViewProtocol
-        
-        else
-        {
-            return false
-        }
-        
-        let panBack:Bool = view.panBack
-        
-        return panBack
     }
 }
--- gattaca/View/Abstract/Basic/ViewParentGesture.swift
+++ gattaca/View/Abstract/Basic/ViewParentGesture.swift
@@ -1,6 +1,140 @@
 import UIKit
 
-extension ViewParent
+extension ViewParent:UIGestureRecognizerDelegate
 {
+    //MARK: selectors
     
+    func actionPanRecognized(sender panGesture:UIPanGestureRecognizer)
+    {
+        let location:CGPoint = panGesture.location(in:self)
+        let xPos:CGFloat = location.x
+        
+        switch panGesture.state
+        {
+        case UIGestureRecognizerState.began,
+             UIGestureRecognizerState.possible:
+            
+            if xPos < kMaxXPanning
+            {
+                self.panningX = xPos
+            }
+            
+            break
+            
+        case UIGestureRecognizerState.changed:
+            
+            if let panningX:CGFloat = self.panningX
+            {
+                var deltaX:CGFloat = xPos - panningX
+                
+                if deltaX > kMaxXDelta
+                {
+                    panRecognizer.isEnabled = false
+                }
+                else
+                {
+                    if deltaX < 0
+                    {
+                        deltaX = 0
+                    }
+                    
+                    guard
+                        
+                        let topView:ViewMain = subviews.last as? ViewMain
+                        
+                        else
+                    {
+                        return
+                    }
+                    
+                    topView.layoutLeft.constant = deltaX
+                }
+            }
+            
+            break
+            
+        case UIGestureRecognizerState.cancelled,
+             UIGestureRecognizerState.ended,
+             UIGestureRecognizerState.failed:
+            
+            if let panningX:CGFloat = self.panningX
+            {
+                let deltaX:CGFloat = xPos - panningX
+                
+                if deltaX > kMinXDelta
+                {
+                    gesturePop()
+                }
+                else
+                {
+                    gestureRestore()
+                }
+            }
+            
+            panningX = nil
+            
+            break
+        }
+    }
+    
+    //MARK: private
+    
+    private func gesturePop()
+    {
+        panRecognizer.isEnabled = true
+        controller.pop(horizontal:ControllerParent.Horizontal.right)
+    }
+    
+    private func gestureRestore()
+    {
+        panRecognizer.isEnabled = true
+        
+        guard
+            
+            let topView:ViewProtocol = subviews.last as? ViewProtocol
+            
+        else
+        {
+            return
+        }
+        
+        topView.layoutLeft.constant = 0
+        
+        UIView.animate(withDuration:kAnimationDuration)
+        {
+            self.layoutIfNeeded()
+        }
+    }
+    
+    //MARK: internal
+    
+    func factoryGesture()
+    {
+        let panRecognizer:UIPanGestureRecognizer = UIPanGestureRecognizer(
+            target:self,
+            action:#selector(actionPanRecognized(sender:)))
+        panRecognizer.delegate = self
+        self.panRecognizer = panRecognizer
+        
+        addGestureRecognizer(panRecognizer)
+    }
+    
+    //MARK: gestureRecognizerDelegate
+    
+    override func gestureRecognizerShouldBegin(
+        _ gestureRecognizer:UIGestureRecognizer) -> Bool
+    {
+        guard
+            
+            let view:ViewProtocol = subviews.last as? ViewProtocol
+            
+        else
+        {
+            return false
+        }
+        
+        let panBack:Bool = view.panBack
+        
+        return panBack
+    }
 }
