e26f0098b0698de194946a6aefea90c62c72f559
vauxhall <zero.griffin@gmail.com>
2017-08-22 22:34:22 -0500

    Layout display mark

--- gattaca/View/Home/VHomeDisplayMark.swift
+++ gattaca/View/Home/VHomeDisplayMark.swift
@@ -2,5 +2,37 @@ import UIKit
 
 class VHomeDisplayMark:View<VHome, MHome, CHome>
 {
+    private weak var imageView:UIImageView!
     
+    required init(controller:CHome)
+    {
+        super.init(controller:controller)
+        isUserInteractionEnabled = false
+        backgroundColor = UIColor.white
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
+        self.imageView = imageView
+        
+        addSubview(imageView)
+        
+        NSLayoutConstraint.equals(
+            view:imageView,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: internal
+    
+    func updateMark(action:MHomeActionProtocol)
+    {
+        imageView.image = action.icon
+    }
 }
