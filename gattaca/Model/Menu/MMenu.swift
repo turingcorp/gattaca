51f46128858c81165b970af3211a6cb0bcaf4030
vauxhall <zero.griffin@gmail.com>
2017-08-23 17:58:20 -0500

    Set initial selected

--- gattaca/Model/Menu/MMenu.swift
+++ gattaca/Model/Menu/MMenu.swift
@@ -2,10 +2,13 @@ import Foundation
 
 class MMenu
 {
+    var selected:MMenu.Order
     let items:[MMenuItemProtocol]
+    private let kInitialSelected:MMenu.Order = MMenu.Order.home
     
     init()
     {
+        selected = kInitialSelected
         items = MMenu.factoryItems()
     }
 }
