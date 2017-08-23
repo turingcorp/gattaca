fe8f68e7158e4716f7b3e59483991ea583a266a1
vauxhall <zero.griffin@gmail.com>
2017-08-23 15:24:47 -0500

    Add item home to factory

--- gattaca/Model/Menu/MMenuFactory.swift
+++ gattaca/Model/Menu/MMenuFactory.swift
@@ -5,8 +5,10 @@ extension MMenu
     class func factoryItems() -> [MMenuItemProtocol]
     {
         let itemMatch:MMenuItemMatch = MMenuItemMatch()
+        let itemHome:MMenuItemHome = MMenuItemHome()
         
         let items:[MMenuItemProtocol] = [
+            itemHome,
             itemMatch]
         
         return items
