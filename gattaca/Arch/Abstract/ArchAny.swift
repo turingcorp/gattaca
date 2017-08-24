752cf66d586fdf46918e3e97f5f5c61b75ca5a8b
vauxhall <zero.griffin@gmail.com>
2017-08-24 13:14:32 -0500

    Implement controller

--- gattaca/Arch/Abstract/ArchAny.swift
+++ gattaca/Arch/Abstract/ArchAny.swift
@@ -4,4 +4,5 @@ struct ArchAny<T:Arch>:Arch
 {
     let viewType:T.V.Type = T.V.self
     let modelType:T.M.Type = T.M.self
+    let controllerType:T.C.Type = T.C.self
 }
--- gattaca/Arch/Concrete/ArchHome.swift
+++ gattaca/Arch/Concrete/ArchHome.swift
@@ -4,7 +4,9 @@ struct ArchHome:Arch
 {
     typealias V = VHome
     typealias M = MHome
+    typealias C = CHome
     
     let viewType:V.Type = VHome.self
     let modelType:M.Type = MHome.self
+    let controllerType:C.Type = CHome.self
 }
