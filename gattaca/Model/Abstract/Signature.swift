56fa266f543883caf41d97d7bbf75ef8f0d69d55
vauxhall <zero.griffin@gmail.com>
2017-08-24 11:48:45 -0500

    Create signature

--- gattaca/Model/Abstract/Signature.swift
+++ gattaca/Model/Abstract/Signature.swift
@@ -1,10 +1,10 @@
 import Foundation
 
-protocol MMenuItemConstructorProtocol
+protocol Signature
 {
     associatedtype V:ViewMain
     associatedtype M:Model<Self.V>
     associatedtype C:Controller<Self.V, Self.M>
     
-    var controller:C.Type { get }
+    var controller:C.Type? { get }
 }
