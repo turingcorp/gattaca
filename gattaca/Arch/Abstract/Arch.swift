1696e212d871e42cfc3cd96edf06952fdcf02bc7
vauxhall <zero.griffin@gmail.com>
2017-08-24 13:13:43 -0500

    Add controller to arch

--- gattaca/Arch/Abstract/Arch.swift
+++ gattaca/Arch/Abstract/Arch.swift
@@ -4,7 +4,9 @@ protocol Arch
 {
     associatedtype V:ViewMain
     associatedtype M:Model<Self.V>
+    associatedtype C:Controller<Self.V, Self.M>
     
     var viewType:V.Type { get }
     var modelType:M.Type { get }
+    var controllerType:C.Type { get }
 }
