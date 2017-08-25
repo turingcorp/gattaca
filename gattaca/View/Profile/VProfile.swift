71acc0a6a9578ffdc20580701e0243fba6abd741
vauxhall <zero.griffin@gmail.com>
2017-08-25 09:48:21 -0500

    Layout viewbar

--- gattaca/View/Profile/VProfile.swift
+++ gattaca/View/Profile/VProfile.swift
@@ -2,5 +2,45 @@ import UIKit
 
 class VProfile:ViewMain
 {
+    private let kBarHeight:CGFloat = 64
     
+    required init(controller:UIViewController)
+    {
+        super.init(controller:controller)
+        
+        guard
+        
+            let controller:CProfile = controller as? CProfile
+        
+        else
+        {
+            return
+        }
+        
+        factoryViews(controller:controller)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: private
+    
+    private func factoryViews(controller:CProfile)
+    {
+        let viewBar:VProfileBar = VProfileBar(controller:controller)
+        
+        addSubview(viewBar)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewBar,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:viewBar,
+            constant:kBarHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewBar,
+            toView:self)
+    }
 }
