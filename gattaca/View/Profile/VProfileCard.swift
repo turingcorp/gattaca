b40e1a7f789553f095e439fdf42d61b0d80b6f77
vauxhall <zero.griffin@gmail.com>
2017-08-25 10:20:13 -0500

    Add border to card

--- gattaca/View/Profile/VProfileCard.swift
+++ gattaca/View/Profile/VProfileCard.swift
@@ -2,11 +2,28 @@ import UIKit
 
 class VProfileCard:View<VProfile, MProfile, CProfile>
 {
+    private let kBorderHeight:CGFloat = 1
+    
     required init(controller:CProfile)
     {
         super.init(controller:controller)
         backgroundColor = UIColor.colourSuccess
         isUserInteractionEnabled = false
+        
+        let border:VBorder = VBorder(
+            colour:UIColor(white:0, alpha:0.2))
+        
+        addSubview(border)
+        
+        NSLayoutConstraint.bottomToBottom(
+            view:border,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:border,
+            constant:kBorderHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:border,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
