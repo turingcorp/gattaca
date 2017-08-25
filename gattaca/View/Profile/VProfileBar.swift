7f67ee0f5e14a8a90784d1bdc995e97d1e5d4b37
vauxhall <zero.griffin@gmail.com>
2017-08-25 09:39:23 -0500

    Add border to bar

--- gattaca/View/Profile/VProfileBar.swift
+++ gattaca/View/Profile/VProfileBar.swift
@@ -2,5 +2,31 @@ import UIKit
 
 class VProfileBar:View<VProfile, MProfile, CProfile>
 {
+    private let kBorderHeight:CGFloat = 1
     
+    required init(controller:CProfile)
+    {
+        super.init(controller:controller)
+        backgroundColor = UIColor.white
+        
+        let border:VBorder = VBorder(
+            colour:UIColor.colourBackgroundDark.withAlphaComponent(0.2))
+        
+        addSubview(border)
+        
+        NSLayoutConstraint.bottomToBottom(
+            view:border,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:border,
+            constant:kBorderHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:border,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
