0c2d750519fc3adacb2f26dd6acd2d9830eb7138
vauxhall <zero.griffin@gmail.com>
2017-08-25 10:18:19 -0500

    Layout view card

--- gattaca/View/Profile/VProfile.swift
+++ gattaca/View/Profile/VProfile.swift
@@ -2,6 +2,10 @@ import UIKit
 
 class VProfile:ViewMain
 {
+    private weak var viewCard:VProfileCard!
+    private weak var layoutCardHeight:NSLayoutConstraint!
+    private let kCardExtraHeight:CGFloat = 80
+    
     required init(controller:UIViewController)
     {
         super.init(controller:controller)
@@ -23,10 +27,32 @@ class VProfile:ViewMain
         return nil
     }
     
+    override func layoutSubviews()
+    {
+        let width:CGFloat = bounds.width
+        let cardHeight:CGFloat = width + kCardExtraHeight
+        layoutCardHeight.constant = cardHeight
+        
+        super.layoutSubviews()
+    }
+    
     //MARK: private
     
     private func factoryViews(controller:CProfile)
     {
+        let viewCard:VProfileCard = VProfileCard(
+            controller:controller)
+        self.viewCard = viewCard
         
+        addSubview(viewCard)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewCard,
+            toView:self)
+        layoutCardHeight = NSLayoutConstraint.height(
+            view:viewCard)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewCard,
+            toView:self)
     }
 }
