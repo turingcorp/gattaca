e77a3117bb084820f93cbc86d3ae969597f3f912
vauxhall <zero.griffin@gmail.com>
2017-08-25 14:02:18 -0500

    Add border

--- gattaca/View/ProfileEdit/VProfileEdit.swift
+++ gattaca/View/ProfileEdit/VProfileEdit.swift
@@ -2,5 +2,13 @@ import UIKit
 
 class VProfileEdit:ViewMain
 {
+    required init(controller:UIViewController)
+    {
+        super.init(controller:controller)
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
