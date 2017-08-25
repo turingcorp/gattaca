b1e643316743b666c6ae7ec35e1b580e8ab3334a
vauxhall <zero.griffin@gmail.com>
2017-08-25 11:02:26 -0500

    Add edit button

--- gattaca/View/Profile/VProfile.swift
+++ gattaca/View/Profile/VProfile.swift
@@ -5,6 +5,7 @@ class VProfile:ViewMain
     private weak var viewCard:VProfileCard!
     private weak var layoutCardHeight:NSLayoutConstraint!
     private let kCardExtraHeight:CGFloat = 80
+    private let kEditHeight:CGFloat = 70
     
     required init(controller:UIViewController)
     {
@@ -40,11 +41,17 @@ class VProfile:ViewMain
     
     private func factoryViews(controller:CProfile)
     {
+        let editHeight_2:CGFloat = kEditHeight / 2.0
+        
         let viewCard:VProfileCard = VProfileCard(
             controller:controller)
         self.viewCard = viewCard
         
+        let viewEdit:VProfileEdit = VProfileEdit(
+            controller:controller)
+        
         addSubview(viewCard)
+        addSubview(viewEdit)
         
         NSLayoutConstraint.topToTop(
             view:viewCard,
@@ -53,6 +60,17 @@ class VProfile:ViewMain
             view:viewCard)
         NSLayoutConstraint.equalsHorizontal(
             view:viewCard,
+            toView:self)
+        
+        NSLayoutConstraint.topToBottom(
+            view:viewEdit,
+            toView:viewCard,
+            constant:-editHeight_2)
+        NSLayoutConstraint.height(
+            view:viewEdit,
+            constant:kEditHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewEdit,
             toView:self)
     }
 }
