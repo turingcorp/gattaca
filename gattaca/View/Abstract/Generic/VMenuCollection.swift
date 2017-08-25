5581302ace4224c5cd49c270539ac542d4b64c72
vauxhall <zero.griffin@gmail.com>
2017-08-25 05:35:28 -0500

    Prevent selection of selected item

--- gattaca/View/Abstract/Generic/VMenuCollection.swift
+++ gattaca/View/Abstract/Generic/VMenuCollection.swift
@@ -123,6 +123,36 @@ extension VMenu:
     
     func collectionView(
         _ collectionView:UICollectionView,
+        shouldSelectItemAt indexPath:IndexPath) -> Bool
+    {
+        let item:MMenuItemProtocol = modelAtIndex(index:indexPath)
+        let order:MMenu.Order = item.order
+        
+        if order == controller.menu.selected
+        {
+            return false
+        }
+        
+        return true
+    }
+    
+    func collectionView(
+        _ collectionView:UICollectionView,
+        shouldHighlightItemAt indexPath:IndexPath) -> Bool
+    {
+        let item:MMenuItemProtocol = modelAtIndex(index:indexPath)
+        let order:MMenu.Order = item.order
+        
+        if order == controller.menu.selected
+        {
+            return false
+        }
+        
+        return true
+    }
+    
+    func collectionView(
+        _ collectionView:UICollectionView,
         didSelectItemAt indexPath:IndexPath)
     {
         collectionView.isUserInteractionEnabled = false
