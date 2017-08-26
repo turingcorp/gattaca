f461d0adb1d7932090f26749c276e7d6c2406378
vauxhall <zero.griffin@gmail.com>
2017-08-26 17:56:24 -0500

    Add method factory views

--- gattaca/View/ProfileEdit/VProfileEdit.swift
+++ gattaca/View/ProfileEdit/VProfileEdit.swift
@@ -2,13 +2,33 @@ import UIKit
 
 class VProfileEdit:ViewMain
 {
+    private let kBarHeight:CGFloat = 64
+    
     required init(controller:UIViewController)
     {
         super.init(controller:controller)
+        
+        guard
+        
+            let controller:CProfileEdit = controller as? CProfileEdit
+        
+        else
+        {
+            return
+        }
+        
+        factoryViews(controller:controller)
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    //MARK: private
+    
+    private func factoryViews(controller:CProfileEdit)
+    {
+        
     }
 }
