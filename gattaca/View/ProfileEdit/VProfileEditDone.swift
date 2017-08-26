5a8f5f70ca7731f87834bd288bf9537423e4839d
vauxhall <zero.griffin@gmail.com>
2017-08-26 18:09:43 -0500

    Add label title

--- gattaca/View/ProfileEdit/VProfileEditDone.swift
+++ gattaca/View/ProfileEdit/VProfileEditDone.swift
@@ -3,5 +3,38 @@ import UIKit
 class VProfileEditDone:View
     <VProfileEdit, MProfileEdit, CProfileEdit>
 {
+    private let kLabelMarginRight:CGFloat = -10
     
+    required init(controller:CProfileEdit)
+    {
+        super.init(controller:controller)
+        
+        let labelTitle:UILabel = UILabel()
+        labelTitle.isUserInteractionEnabled = false
+        labelTitle.translatesAutoresizingMaskIntoConstraints = false
+        labelTitle.textAlignment = NSTextAlignment.right
+        labelTitle.font = UIFont.bold(size:15)
+        labelTitle.backgroundColor = UIColor.clear
+        labelTitle.textColor = UIColor.colourSuccess
+        labelTitle.text = String.localizedView(
+            key:"VProfileEditDone_labelTitle")
+        
+        addSubview(labelTitle)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:labelTitle,
+            toView:self)
+        NSLayoutConstraint.leftToLeft(
+            view:labelTitle,
+            toView:self)
+        NSLayoutConstraint.rightToRight(
+            view:labelTitle,
+            toView:self,
+            constant:kLabelMarginRight)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
