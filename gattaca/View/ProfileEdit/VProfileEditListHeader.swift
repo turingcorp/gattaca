b69645d284fc2fd0d3a20eba84f6f0d309a38235
vauxhall <zero.griffin@gmail.com>
2017-08-27 11:43:57 -0500

    Add inits to cell/header

--- gattaca/View/ProfileEdit/VProfileEditListHeader.swift
+++ gattaca/View/ProfileEdit/VProfileEditListHeader.swift
@@ -2,5 +2,15 @@ import UIKit
 
 class VProfileEditListHeader:UICollectionReusableView
 {
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
