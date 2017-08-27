f28908b18dce6c88a53953518418f7968ecb52d9
vauxhall <zero.griffin@gmail.com>
2017-08-27 12:01:30 -0500

    Add picture properties

--- gattaca/Model/ProfileEdit/MProfileEditItemPictures.swift
+++ gattaca/Model/ProfileEdit/MProfileEditItemPictures.swift
@@ -2,5 +2,7 @@ import UIKit
 
 class MProfileEditItemPictures:MProfileEditItemProtocol
 {
-    
+    let headerTitle:String? = nil
+    let reusableIdentifier:String = VProfileEditListCellPictures.reusableIdentifier
+    let height:CGFloat = 200
 }
