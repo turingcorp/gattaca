cda6e935cb3b49db6314c64fbb26b2e20bcc3979
vauxhall <zero.griffin@gmail.com>
2017-08-28 11:15:53 -0500

    Create item name

---
+++ gattaca/Model/ProfileEdit/MProfileEditItemName.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class MProfileEditItemName:MProfileEditItemProtocol
+{
+    let headerTitle:String? = nil
+    let reusableIdentifier:String = VProfileEditListCellPictures.reusableIdentifier
+    let height:CGFloat = 60
+}
