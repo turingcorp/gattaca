713805b07553b573b980c006ec46f778cd9144f5
vauxhall <zero.griffin@gmail.com>
2017-08-28 11:35:49 -0500

    Factory items

--- gattaca/Model/ProfileEdit/MProfileEdit.swift
+++ gattaca/Model/ProfileEdit/MProfileEdit.swift
@@ -2,5 +2,12 @@ import Foundation
 
 class MProfileEdit:Model<VProfileEdit>
 {
+    let items:[MProfileEditItemProtocol]
     
+    required init(session:MSession)
+    {
+        items = MProfileEdit.factoryItems()
+        
+        super.init(session:session)
+    }
 }
