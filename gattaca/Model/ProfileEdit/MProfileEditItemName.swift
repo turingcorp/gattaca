9ce3152f46190f70e45e3b10b45ab97a7fc358c3
vauxhall <zero.griffin@gmail.com>
2017-08-28 11:27:20 -0500

    Add header title

--- gattaca/Model/ProfileEdit/MProfileEditItemName.swift
+++ gattaca/Model/ProfileEdit/MProfileEditItemName.swift
@@ -2,7 +2,7 @@ import UIKit
 
 class MProfileEditItemName:MProfileEditItemProtocol
 {
-    let headerTitle:String? = nil
-    let reusableIdentifier:String = VProfileEditListCellPictures.reusableIdentifier
+    let headerTitle:String? = String.localizedModel(key:"MProfileEditItemName_headerTitle")
+    let reusableIdentifier:String = VProfileEditListCellName.reusableIdentifier
     let height:CGFloat = 60
 }
