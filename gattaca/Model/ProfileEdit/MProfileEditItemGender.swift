e0e6973e5600e58e4e5155200bf22d64d9c8e81c
vauxhall <zero.griffin@gmail.com>
2017-08-29 13:39:37 -0500

    Create item gender

---
+++ gattaca/Model/ProfileEdit/MProfileEditItemGender.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class MProfileEditItemGender:MProfileEditItemProtocol
+{
+    let headerTitle:String? = String.localizedModel(key:"MProfileEditItemGender_headerTitle")
+    let reusableIdentifier:String = VProfileEditListCellGender.reusableIdentifier
+    let height:CGFloat = 90
+}
