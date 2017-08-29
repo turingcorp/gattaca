9a6fcb8c5104173999b511157deb6962400154be
vauxhall <zero.griffin@gmail.com>
2017-08-29 11:47:25 -0500

    Create item age

---
+++ gattaca/Model/ProfileEdit/MProfileEditItemAge.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class MProfileEditItemAge:MProfileEditItemProtocol
+{
+    let headerTitle:String? = String.localizedModel(key:"MProfileEditItemAge_headerTitle")
+    let reusableIdentifier:String = VProfileEditListCellName.reusableIdentifier
+    let height:CGFloat = 90
+}
