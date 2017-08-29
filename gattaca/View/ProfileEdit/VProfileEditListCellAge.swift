ca20c6da61b43a85f5a050424ef0f568c950c8fc
vauxhall <zero.griffin@gmail.com>
2017-08-29 11:48:37 -0500

    Create cell age

---
+++ gattaca/View/ProfileEdit/VProfileEditListCellAge.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class VProfileEditListCellAge:VProfileEditListCell
+{
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+}
