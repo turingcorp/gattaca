3d09a974585f89b5979b77f87f36c9c636a5d520
vauxhall <zero.griffin@gmail.com>
2017-08-29 12:11:42 -0500

    Create cell gender

---
+++ gattaca/View/ProfileEdit/VProfileEditListCellGender.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class VProfileEditListCellGender:VProfileEditListCell
+{
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+}
